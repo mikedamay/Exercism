c954b152592e5905bebce1cf8dfec678cb1d4edb
mikedamay <mikemay@blueyonder.co.uk>
2018-08-30 09:19:30 +0100

    rna trasnscription - my solution

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -1,9 +1,19 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class RnaTranscription
 {
     public static string ToRna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return string.Concat(nucleotide.Select(c => map[c]));
     }
+
+    private static IReadOnlyDictionary<char, char> map = new Dictionary<char, char>
+    {
+        { 'C', 'G' },
+        { 'G', 'C' },
+        { 'T', 'A' },
+        { 'A', 'U' }
+    };
+    
 }
