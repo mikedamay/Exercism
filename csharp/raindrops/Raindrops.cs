981a0e06e1ede5d830e47c3c10fd979bd599feac
mikedamay <mikemay@blueyonder.co.uk>
2018-09-05 04:45:10 +0100

    tweaked Raindrops - no longer matches published solution

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -5,7 +5,7 @@ public static class Raindrops
 {
     public static string Convert(int number)
     {
-        var list = Enumerable.Range(1, number / 2).Where(
+        var list = new int[] {3, 5, 7}.Where(n => n < number).Where(
                 n => number % n == 0).Where(nn => nn == 3 || nn == 5 || nn == 7).Append(number)
             .Select(nnn => nnn == 3 ? "Pling" : nnn == 5 ? "Plang" : nnn == 7 ? "Plong" : nnn.ToString())
             .ToArray();
--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> inputs, int max)
         => inputs.SelectMany(
             input => Enumerable.Range(1, (max - 1) / input).Select(numTimes => numTimes * input)
-            , (numtimes, multiple) => multiple).ToHashSet().Sum();
+            , (numtimes, multiple) => multiple).Distinct().Sum();
 }
