4728f73b51fb335ae9d0ca485abfa61e7fc7e04d
mikedamay <mikemay@blueyonder.co.uk>
2018-09-30 06:14:22 +0100

    better version of raindrops

---
+++ csharp/raindrops/Better.cs
@@ -0,0 +1,9 @@
+using System.Linq;
+
+public static class Raindrops_Better
+{
+    public static string Convert(int number)
+        => new (string sound, int value)[] {("Pling", 3), ("Plang", 5), ("Plong", 7),}
+            .Where(p => number % p.value == 0).Select(p => p.sound)
+            .DefaultIfEmpty(number.ToString()).Aggregate((a, b) => a + b);
+}
