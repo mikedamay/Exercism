d417eda05ea65416c03476907d1eccd802665448
mikedamay <mikemay@blueyonder.co.uk>
2018-10-17 14:00:21 +0100

    Armstrong

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+
+        string ns = number.ToString();
+        uint exponent = (uint)ns.Length;
+        return number == ns.Select(c => IntPow(c - 0x30, exponent)).Aggregate((m, n) => m + n);
+    }
+    private static int IntPow(int x, uint pow)
+    {
+        int res = 1;
+        for (uint ii = 0; ii < pow; ii++)
+        {
+            res *= x;
+        }
+
+        return res;
+    }
+}
