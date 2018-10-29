0974096575b8bea7a05047039b87afebf67cf5ad
mikedamay <mikemay@blueyonder.co.uk>
2018-10-29 09:36:07 +0000

    first shot at SecretHandshake

--- csharp/secret-handshake/SecretHandshake.cs
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class SecretHandshake
 {
     public static string[] Commands(int commandValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var commands = GenerateCommands(commandValue);
+        return ((commandValue & (0b10000)) != 0 ? commands.Reverse() : commands).ToArray();
+    }
+
+    private static IEnumerable<string> GenerateCommands(int commandValue)
+    {
+        for (int ii = 0; ii < 4; ii++, commandValue >>= 1 )
+        {
+            if ((commandValue & 1) == 1)
+            {
+                switch (ii)
+                {
+                    case 0:
+                        yield return "wink";
+                        break;
+                    case 1:
+                        yield return "double blink";
+                        break;
+                    case 2:
+                        yield return "close your eyes";
+                        break;
+                    case 3:
+                        yield return "jump";
+                        break;
+                }
+            }
+        }
     }
 }
