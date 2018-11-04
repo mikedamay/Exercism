485e7de04eb04ce9bfec605346736581a3e7ded0
mikedamay <mikemay@blueyonder.co.uk>
2018-11-04 09:12:09 +0000

    1st draft Etl

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class Etl
 {
     public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+      => old.SelectMany(kvp => kvp.Value.Select(c => (c.ToLower(), kvp.Key))).ToDictionary(p => p.Item1, p => p.Item2);
 }
