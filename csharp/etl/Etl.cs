691ec91aafc3e14cf6767ca48ffbae25e3aab0a8
mikedamay <mikemay@blueyonder.co.uk>
2018-11-04 09:14:43 +0000

    submission for etl

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -5,5 +5,5 @@ using System.Linq;
 public static class Etl
 {
     public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
-      => old.SelectMany(kvp => kvp.Value.Select(c => (c.ToLower(), kvp.Key))).ToDictionary(p => p.Item1, p => p.Item2);
+      => old.SelectMany(kvp => kvp.Value, (kvp, c) => (c.ToLower(), kvp.Key)).ToDictionary(p => p.Item1, p => p.Item2);
 }
