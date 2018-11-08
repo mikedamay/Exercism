568151f61a58e3b90baafb38a2ac3efb1180665f
mikedamay <mikemay@blueyonder.co.uk>
2018-11-08 14:54:13 +0000

    1st draft of ScrabbleScore

--- csharp/scrabble-score/ScrabbleScore.cs
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ScrabbleScore
 {
+    private static IReadOnlyDictionary<char, int> scores = new Dictionary<char, int>
+    {
+        {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1},
+        {'D', 2}, {'G', 2},
+        {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3},
+        {'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4},
+        {'K', 5},
+        {'J', 8}, {'X', 8},
+        {'Q', 10}, {'Z', 10}
+    };
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return input
+            .Select(letter => scores[Char.ToUpper(letter)])
+            .DefaultIfEmpty(0)
+            .Aggregate((word_score, letter_score) => word_score + letter_score);
     }
 }
