3c13d55ed8dddcfa11e7cda00cd48f6b64a862d9
mikedamay <mikemay@blueyonder.co.uk>
2018-11-12 08:16:27 +0000

    making guidelines for clock

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+/*
+        // I prefer alphabet as a literal
+        string alphabet()
+        {
+            StringBuilder sb = new StringBuilder();
+            return Enumerable
+                .Range(97, 26)
+                .Select(n => (char)n)
+                .Aggregate(sb, (chs, ch) => chs.Append(ch))
+                .ToString();
+            
+        }
+*/
+
+        return !"abcdefghijklmnopqrstuvwxyz".Except(input.Replace(" ", string.Empty).ToLower()).Any();
     }
 }
