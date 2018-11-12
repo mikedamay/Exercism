221c10e85edf5120101492ab2d2c1c73eae33102
mikedamay <mikemay@blueyonder.co.uk>
2018-11-12 07:29:56 +0000

    Grains - 1st draft

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -1,14 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Grains
 {
     public static ulong Square(int n)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
+        => n <= 0 || n > 64 ? throw new ArgumentOutOfRangeException("invalid input of 0") : 1UL << (n - 1);
     public static ulong Total()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enumerable.Range(1, 64).Select(n => Square(n)).Aggregate((m, n) => m + n);
     }
+
 }
