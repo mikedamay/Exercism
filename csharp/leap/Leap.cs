35209134180d62d001de8b76a233fbb785d08e32
mikedamay <mikemay@blueyonder.co.uk>
2018-11-15 06:37:51 +0000

    Leap - add some basic instructions

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -5,3 +5,10 @@ public static class Leap
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 }
+/*
+Exercises in exercism are designed to be run by unit tests. Pay close attention to the instructions 
+about running the tests, and follow the link there to the C# language page if you're running into trouble. 
+Your submission is expected to return a value rather than outputting it to the console. 
+Your initial download should have included a Leap.cs file with a template for you to get started with. 
+Once you've got that figured out, go ahead and submit a new solution. :-)
+*/
