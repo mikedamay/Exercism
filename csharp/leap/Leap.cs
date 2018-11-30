578e558fae98048a0db18ee1b00ef428144cf588
mikedamay <mikemay@blueyonder.co.uk>
2018-11-30 07:49:43 +0000

    Leap docs

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -12,3 +12,21 @@ Your submission is expected to return a value rather than outputting it to the c
 Your initial download should have included a Leap.cs file with a template for you to get started with. 
 Once you've got that figured out, go ahead and submit a new solution. :-)
 */
+
+/*
+Instead of having an if-statement and explicitly returning `true` or `false`, you could also just return the expression in the if-statement.
+
+Consider the following two bits of code, which are functionally equivalent:
+```
+if (x > 1)
+{
+    return true;
+}
+
+return false;
+```
+and
+```
+return x > 1;
+```
+*/
