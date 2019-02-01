cf729bf94657c62ca9aff4ea97544a0e47b793a6
mikedamay <mikemay@blueyonder.co.uk>
2019-02-01 10:18:28 +0000

    binary-search

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿﻿using System;
+
+// davidfrogley's solution
+public static class BinarySearch
+{
+	public static int Find(int[] dataArg, int valueArg)
+	{
+		int Find(int[] data, int value, int left, int right)
+		{
+			var middle = (left + right) / 2;
+
+			if (left == middle || middle == right)
+			{
+				return data[left] == value ? left : data[right] == value ? right : -1;
+			}
+
+			if (data[middle] == value)
+			{
+				return middle;
+			}
+
+			if (value < data[middle])
+			{
+				return Find(data, value, left, middle);
+			}
+
+			if (value > data[middle])
+			{
+				return Find(data, value, middle, right);
+			}
+
+			return -1;
+		}
+		if (dataArg.Length == 0)
+		{
+			return -1;
+		}
+
+		return Find(dataArg, valueArg, 0, dataArg.Length - 1);
+	}
+
+
+}
