f2ae764d0d4cfc2b5b00a1311cd88d1f35ed490d
mikedamay <mikemay@blueyonder.co.uk>
2019-02-05 09:05:09 +0000

    docs for Leap

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -54,3 +54,30 @@ public class Robot
     }
 }
 
+/*
+I hold the opinion (I did say the comments were for your consideration - which of course includes ignoring them) that you should favour expressive code over non-expressive code and that this applies at all levels, other things being equal.
+
+Expressiveness:
+
+One aspect of expression is the relationship of the code to the business rules.  In this case we have been told to generate 2 letters and 3 numbers. If the maintainer sees 2 lines of code relating to letters followed by 3 lines of code relating to numbers they won’t even stop to consider it.  With your (perfectly good - but not meeting my expressiveness criterion - solution) the number 5 is involved which is good but then there is some question of a special condition of less than 2 that they will have to stop and parse.  They also have to recollect their ascii codes to parse the rest of it.
+
+Something like:
+```
+  newName += (char)rnd.Next(‘A’, ‘Z’);
+  newName += (char)rnd.Next(‘A’, ‘Z’);
+  newName += (char)rnd.Next(‘0’, ‘9’);
+  newName += (char)rnd.Next(‘0’, ‘9’);
+  newName += (char)rnd.Next(‘0’, ‘9’);
+```
+is worth considering.
+
+At all Levels:
+
+Conventional wisdom is that variable and member names should be expressive and that operations can be abstracted in a well named method.  This is, of course, true but I see no reason not to extend this to implementation details. 
+
+Other Things Being Equal:
+
+On the other side of the equation is the amount of reading and (to a lesser extent) writing that coders have to do.  If the code becomes too verbose or noisy then it ceases to be expressive and can become burdensome.
+
+Interesting code is written once but read many times.
+ */
