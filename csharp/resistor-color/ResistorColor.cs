9d3f7ec3e5db1d1a83a0d886822f2d95fe2ad51d
mikedamay <mikemay@blueyonder.co.uk>
2019-02-19 14:03:48 +0000

    resistor-color final

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Sockets;
 
 public static class ResistorColor
 {
+    enum Color
+    {
+        black,
+        brown,
+        red,
+        orange,
+        yellow,
+        green,
+        blue,
+        violet,
+        grey,
+        white
+        
+    }
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return (int)Enum.Parse(typeof(Color), color);
     }
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enum.GetNames(typeof(Color));        
     }
 }
