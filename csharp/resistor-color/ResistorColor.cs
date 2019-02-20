93428c39ea834edb0420674562e9b89f059ef7a1
mikedamay <mikemay@blueyonder.co.uk>
2019-02-20 06:37:21 +0000

    gigasecond doc

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -38,3 +38,11 @@ I like the `ReadOnlyDictionary` with initializers.
 As an alternative you could have used en Enum of colors.  I think the choice depends on how the values will be used in the program which is not obvious from the exercise notes.
 
 */
+
+/*
+An effective solution.
+
+Have a look at `Enum.Parse()` and `Enum.GetValues()` and decide whether you would prefer a version using these member methods rather than LINQ.
+
+I would expect LINQ to be slower but you can't always tell.
+*/
