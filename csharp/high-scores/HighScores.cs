c7424909475591c376c566112f290cdac43fd023
mikedamay <mikemay@blueyonder.co.uk>
2019-02-21 08:20:07 +0000

    high scores - complete

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -4,33 +4,36 @@ using System.Linq;
 
 public class HighScores
 {
+    private readonly List<int> list;
     public HighScores(List<int> list)
     {
-        throw new NotImplementedException();
+        this.list = list;
     }
 
     public List<int> Scores()
     {
-        throw new NotImplementedException();
+        return list;
     }
 
     public int Latest()
     {
-        throw new NotImplementedException();
+        return list.Last();
     }
 
     public int PersonalBest()
     {
-        throw new NotImplementedException();
+        return list.Max();
     }
 
     public List<int> PersonalTop()
     {
-        throw new NotImplementedException();
+        return list.OrderByDescending(n => n).Take(3).ToList();
     }
 
     public string Report()
     {
-        throw new NotImplementedException();
+        var best = $"Your latest score was {Latest()}. That's your personal best!";
+        var lesser = $"Your latest score was {Latest()}. That's {PersonalBest() - Latest()} short of your personal best!";
+        return Latest() == PersonalBest() ? best : lesser;
     }
 }
