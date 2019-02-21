220d7015130408674215df37c6cfac2a9ac3eee2
mikedamay <mikemay@blueyonder.co.uk>
2019-02-21 09:05:18 +0000

    high scores - docs

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -37,3 +37,42 @@ public class HighScores
         return Latest() == PersonalBest() ? best : lesser;
     }
 }
+
+namespace MyTests 
+{    
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class MyTests
+    {
+        [Fact(Skip = "")]
+        public void Personal_best()
+        {
+            Assert.Throws<InvalidOperationException>(
+                () =>
+                {
+                    var sut = new HighScores(new List<int> { }).PersonalBest();
+                });
+        }
+    }
+    
+}
+/*
+This is an effective solution.
+
+A couple of comments:
+
+in `PersonalBest()` initialising `TopScores` with a brand new list is a waste of effort as you immediately overwrite it with the list returned from `PeresonalTop()`.
+
+`PersonalTop()[0]` is a valid expression if you wanted to shorten the implementation.
+
+You've used `Enumerable.Last()` which is a LINQ method.  Also available are:
+```
+Enumerable.Max()
+Enumerable.OrderByDescending()
+Enumerable.Take()
+Enumerable.ToList()
+```
+This exercise is useful as a gentle introduction to LINQ.  In particular use of LINQ methods avoids mutating the list.  Mutability is very much out of fashion at present.
+
+Make whatever changes you want, ask questions, argue as you see fit.
