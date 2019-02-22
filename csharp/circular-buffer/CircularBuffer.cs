57d415c0fee4331342bdde41a1364d187fcff2fd
mikedamay <mikemay@blueyonder.co.uk>
2019-02-22 13:07:55 +0000

    circular buffer - first draft

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -1,29 +1,78 @@
 ﻿using System;
+using System.Diagnostics;
 
 public class CircularBuffer<T>
 {
+    private readonly int capacity;
+    private readonly T[] buffer;
+    private int lastInIdx;
+    private int nextOutIdx;
+    private int usage;
     public CircularBuffer(int capacity)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        this.capacity = capacity;
+        buffer = new T[capacity];
+        Clear();
     }
 
     public T Read()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (usage == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T rtn = buffer[nextOutIdx];
+        buffer[nextOutIdx] = default(T);
+        Increment(ref nextOutIdx);
+        usage--;
+        return rtn;
     }
 
     public void Write(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (usage == capacity)
+        {
+            throw new InvalidOperationException();
+        }
+        WriteOrOverwrite(value);
+        usage++;
     }
 
     public void Overwrite(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (usage < capacity)
+        {
+            Write(value);
+            return;
+        }
+        WriteOrOverwrite(value);
+        Increment(ref nextOutIdx);
+    }
+
+    private void WriteOrOverwrite(T value)
+    {
+        Increment(ref lastInIdx);
+        buffer[lastInIdx] = value;
     }
 
     public void Clear()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        usage = 0;
+        lastInIdx = capacity - 1;
+        nextOutIdx = 0;
+        for (int ii = 0; ii < capacity; ii++)
+        {
+            buffer[ii] = default(T);
+        }
     }
+
+    private void Increment(ref int idx)
+    {
+        if (++idx == capacity)
+        {
+            idx = 0;
+        }
+    }
+
 }
