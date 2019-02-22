0879688f086c6cc57d226db9ee8b32c02c7ddbf9
mikedamay <mikemay@blueyonder.co.uk>
2019-02-22 09:58:11 +0000

    space-age docs

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -58,5 +58,24 @@ public class SpaceAge
     }
 }
 /*
-This solution is more complex than I would expect in a commercial situation.  However I guess that this is because you are investigating the highways and byways of the language and ecosystem.  If not, let me know and we can discuss.
+This solution is more complex than I would expect in a commercial situation.  
+However I guess that this is because you are investigating the highways and 
+byways of the language and ecosystem.  If not, let me know and we can discuss.
 */
+/*
+Minimum-Necessary
+
+"we can go to a stopper" - this is very true but in spite of that agile methods favour doing the minimum necessary and refactoring as circumstances change.
+
+Risks
+* The risk of minimum-necessary is that a rewrite will be necessary and that is a risk the proponents, like me, of minimum-necessary are prepared to take.  
+
+Costs
+1. The cost of NOT doing minimum-necessary is that you constrain maintainers who may want to change the implementation of the class or modify it in some other way.  
+2. In addition you make it harder for them to reason about the code as they will be considering the possibility of sub-classes. 
+
+I see that this directly clashes with the open-close principle.  I must re-read Uncle Bob / Bertrand Meyer (who knew).  I don't think that it means that you can't change behaviour of a class to reflect a change in circumstances.
+
+For me it's important that the code communicates the maximum amount about what it does and what else it might effect.  Code is generally written/modified a few times and read many more.
+
+All this just because some people want to bring Pluto back as a planet.
