218715f28a448ff72b68c0e45103ffeea5dfff61
mikedamay <mikemay@blueyonder.co.uk>
2019-02-24 08:59:57 +0000

    acronym - 1st draft

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using System.Text;
 
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        bool IsWordSeparator(char c) => !(char.IsLetter(c) || c == '\'');
+        var sb = new StringBuilder();
+        if (char.IsLetter(phrase[0]))
+            sb.Append(char.ToUpper(phrase[0]));
+        for (int ii = 1; ii < phrase.Length; ii++)
+        {
+            if (char.IsLetter(phrase[ii]) && IsWordSeparator(phrase[ii - 1]))
+                sb.Append(char.ToUpper(phrase[ii]));
+        }
+
+        return sb.ToString();
     }
 }
