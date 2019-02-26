c6ce5185a9a1ba61a814fb27a5d0a8cb71832fc2
mikedamay <mikemay@blueyonder.co.uk>
2019-02-26 06:21:53 +0000

    high-score docs

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -7,7 +7,9 @@ public static class TwoFer
   }
 }
 /*
-There are a few idioms you should be aware of:
+An effective solution
+ 
+There are a few idioms you need to be aware of:
 * null coalescence operator `var expr = "something or nothing"; var default_val = "nothing"; var foo = expr ?? default_val;`
 * string interpolation `var expr = "abc"; var foo = $"this is {expr}";`
 * default parameter can be something other than null - `void foo(string bar = "you"){}`
