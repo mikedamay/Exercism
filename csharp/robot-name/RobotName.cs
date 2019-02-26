a116f9a1c0e7e44eaf8172ede261d9b4f09c2b05
mikedamay <mikemay@blueyonder.co.uk>
2019-02-26 07:09:39 +0000

    RobotName - correct user solution committed in error

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,39 +1,83 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 public class Robot
 {
-    public string name;
-    private object random;
-    private object rng;
-    private string[] text = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-    private string[] digit = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-   
+    private string name = null;
+    private readonly Namer namer = new Namer();
     public string Name
     {
         get
         {
-            return name;
-        }
+            if (name == null)
+            {
+                name = namer.NewName();
+            }
 
-        set
-        {
-            name = (GetRandomCharacter() + GetRandomCharacter () + 
+            return name;
         }
     }
 
     public void Reset()
     {
-        
+        name = null;
     }
 
-    public static char GetRandomCharacter(string text, Random rng)
+
+    private class Namer
     {
-        int index = rng.Next(text.Length);
-        return text[index];
-    }
-    public static char GetRandomDigit(string digit, Random rng)
-    {
-        int index = rng.Next(digit.Length);
-        return digit[index];
+        private static Random generator = new Random((int)(DateTime.Now.Ticks % Int64.MaxValue));
+        private static ISet<string> existingNames = new HashSet<string>();
+        internal string NewName()
+        {
+            string candidateName;
+            do
+            {
+                candidateName = GenerateCandidateName();
+            } while (!IsUnique(candidateName));
+
+            existingNames.Add(candidateName);
+            return candidateName;
+        }
+
+        private string GenerateCandidateName()
+        {
+            var sb = new StringBuilder();
+            sb.Append((char)(generator.Next() % 26 + 'A'));
+            sb.Append((char)(generator.Next() % 26 + 'A'));
+            sb.Append((generator.Next() % 1000).ToString("000"));
+            return sb.ToString();
+        }
+
+        private bool IsUnique(string name) => !existingNames.Contains(name);
     }
 }
+
+/*
+I hold the opinion (I did say the comments were for your consideration - which of course includes ignoring them) that you should favour expressive code over non-expressive code and that this applies at all levels, other things being equal.
+
+Expressiveness:
+
+One aspect of expression is the relationship of the code to the business rules.  In this case we have been told to generate 2 letters and 3 numbers. If the maintainer sees 2 lines of code relating to letters followed by 3 lines of code relating to numbers they won’t even stop to consider it.  With your (perfectly good - but not meeting my expressiveness criterion - solution) the number 5 is involved which is good but then there is some question of a special condition of less than 2 that they will have to stop and parse.  They also have to recollect their ascii codes to parse the rest of it.
+
+Something like:
+```
+  newName += (char)rnd.Next(‘A’, ‘Z’);
+  newName += (char)rnd.Next(‘A’, ‘Z’);
+  newName += (char)rnd.Next(‘0’, ‘9’);
+  newName += (char)rnd.Next(‘0’, ‘9’);
+  newName += (char)rnd.Next(‘0’, ‘9’);
+```
+is worth considering.
+
+At all Levels:
+
+Conventional wisdom is that variable and member names should be expressive and that operations can be abstracted in a well named method.  This is, of course, true but I see no reason not to extend this to implementation details. 
+
+Other Things Being Equal:
+
+On the other side of the equation is the amount of reading and (to a lesser extent) writing that coders have to do.  If the code becomes too verbose or noisy then it ceases to be expressive and can become burdensome.
+
+Interesting code is written once but read many times.
+ */
