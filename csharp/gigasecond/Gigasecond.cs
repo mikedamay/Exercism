ef2b7d3bec318231257f84e0525ee95bf5533522
mikedamay <mikemay@blueyonder.co.uk>
2019-03-09 07:46:14 +0000

    gigasecond - docs

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -9,7 +9,11 @@ public static class Gigasecond
 }
 
 /*
+Review Points: 
+ 
 Good solution
+
+Discussion Points:
 
 It could be useful for you to know about the csharp comma separator _ as in `int num = 1_000_000_000;`
 
