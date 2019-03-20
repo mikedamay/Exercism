aece1cfdceb955c3031dce8633b7ef36a86bd1a3
mikedamay <mikemay@blueyonder.co.uk>
2019-03-20 06:16:39 +0000

    isogram - docs

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -12,3 +12,13 @@ public static class Isogram
             .Any(g => g.Count() > 1);
     }
 }
+
+/*
+Review Points:
+
+Good solution
+
+Discussion Points:
+
+You might want to think about a LINQ based solution (or you might not see the point in which case let me know).  Use of `Distinct()` or `GroupBy()` indicate two different ways to approach this.
+*/
