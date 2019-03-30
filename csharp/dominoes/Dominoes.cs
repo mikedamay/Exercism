1f80d637cdf2f2ce79c9b8ab0009c826c1863e56
mikedamay <mikemay@blueyonder.co.uk>
2019-03-30 04:25:07 +0000

    dominoes - functional WIP

--- csharp/dominoes/Dominoes.cs
+++ csharp/dominoes/Dominoes.cs
@@ -4,46 +4,50 @@ using System.Linq;
 
 public static class Dominoes
 {
-    private static bool result = false;
     public static bool CanChain(IEnumerable<(int, int)> dominoes)
     {
-        if (!dominoes.Any())
-            return true;
-        ImmutableList<(int, int)> bag = ImmutableList<(int, int)>.Empty.AddRange(dominoes).RemoveAt(0);
-        ImmutableList<(int, int)> hand = ImmutableList<(int, int)>.Empty.Add(dominoes.First());
-        return PlayStone(bag, hand);
+        ImmutableList<(int, int)> bag() => ImmutableList<(int, int)>.Empty.AddRange(dominoes).RemoveAt(0);
+        ImmutableList<(int, int)> hand() => ImmutableList<(int, int)>.Empty.Add(dominoes.First());
+        return !dominoes.Any() ? true : PlayStone(bag(), hand());
     }
 
-    private static bool PlayStone(ImmutableList<(int, int)> bag, ImmutableList<(int, int)> hand)
+    private static bool PlayStone(ImmutableList<(int, int)> bag, ImmutableList<(int, int)> hand) =>
+         bag.IsEmpty ? hand.First().Item1 == hand.Last().Item2 :
+            GetCandidates(hand.Last(), bag).Any() 
+               && HandleCandidate(ImmutableList<(int, int)>.Empty.AddRange(GetCandidates(hand.Last(), bag)), bag, hand);
+
+    private static bool HandleCandidate(ImmutableList<(int, int)> candidates, ImmutableList<(int, int)> bag, ImmutableList<(int, int)> hand)
     {
-        if (bag.IsEmpty)
-        {
-            result = hand.First().Item1 == hand.Last().Item2;
-            return result;
-        }
+        (int, int) candidateStone() => candidates.First();
+        (int, int) reverseStone() 
+            => candidateStone().Item2 == hand.Last().Item2 ? (candidateStone().Item2, candidateStone().Item1) :
+            candidateStone();
+        bool DoPlayStone((int, int) stone) => PlayStone(bag.Remove(stone), hand.Add(stone));
         
-        var candidates = GetCandidates(hand.Last(), bag);
         if (!candidates.Any())
-        {
-            result = false;
-            return result;
-        }
-        foreach (var candidateStone in candidates)
-        {
-            if (candidateStone.Item2 == hand.Last().Item2)
-            {
-                if (PlayStone(bag.Remove(candidateStone), hand.Add((candidateStone.Item2, candidateStone.Item1))))
-                    return true;
+            return false;
+/*
+        return candidateStone().Item2 == hand.Last().Item2
+            ? DoPlayStone((candidateStone().Item2, candidateStone().Item1)) ||
+              HandleCandidate(candidates.RemoveAt(0), bag, hand)
+            : DoPlayStone(candidateStone()) || HandleCandidate(candidates.RemoveAt(0), bag, hand);
+*/
 
-            }
-            else
-            {
-                if (PlayStone(bag.Remove(candidateStone), hand.Add(candidateStone)))
-                    return true;
-            }
+        return PlayStone(bag.Remove(candidateStone()), hand.Add(reverseStone())) || HandleCandidate(candidates.RemoveAt(0), bag, hand);
+/*
+        if (candidateStone().Item2 == hand.Last().Item2)
+        {
+            if (PlayStone(bag.Remove(candidateStone()), hand.Add((candidateStone().Item2, candidateStone().Item1))))
+                return true;            
+        }
+        else
+        {
+            if (PlayStone(bag.Remove(candidateStone()), hand.Add(candidateStone())))
+                return true;
         }
 
-        return false;
+        return HandleCandidate(candidates.RemoveAt(0), bag, hand);
+*/
     }
 
     private static IEnumerable<(int, int)> GetCandidates((int, int) stoneToMatch, ImmutableList<(int, int)> bag)
