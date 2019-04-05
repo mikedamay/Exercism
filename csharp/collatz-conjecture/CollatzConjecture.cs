203e78c17b906b30f549b7549f967031a6f41b5b
mikedamay <mikemay@blueyonder.co.uk>
2019-04-05 11:12:13 +0100

    collatz-conjecture - first draft

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,18 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        int CountStep(int num)
+        {
+            if (num == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + CountStep(num % 2 == 0 ? num / 2 : num * 3 + 1);
+            }
+        }
+
+        return number <= 0 ? throw new ArgumentException() : CountStep(number);
     }
 }
