64a990ecb88fc65a6fcdfea55adb0d87d996fcef
mikedamay <mikemay@blueyonder.co.uk>
2019-04-30 10:20:02 +0100

    haskell/acronym 1st draft - all tests pass

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,4 +1,44 @@
 module Acronym (abbreviate) where
 
+import Data.Char (toUpper, isUpper, isLower)
+
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate xs = map (toUpper) (concat [ (significantChar x) | x <- split (" " ++ xs)])
+
+significantChar :: [Char] -> [Char]
+significantChar xs = head xs : (if filter (isLower) (tail xs) /= [] then filter (isUpper) (tail xs) else [])
+
+
+-- significantChars [] = []
+-- significantChars (x:[]) = if isUpper [x] then x else []
+-- significantChars (x:y:xs) = significantChars y:xs ++ (if (isUpper x) && not (isUpper y) then x else [])
+
+rev [] = []
+rev (x:[]) = [x]
+rev (x:xs) = (rev xs) ++ [x]
+
+nosp [] = []
+nosp (' ':[]) = []
+nosp (' ':xs) = nosp(xs)
+nosp (x:[]) = [x]
+nosp (x:xs) = [x] ++ nosp(xs)
+
+
+split :: [Char] -> [[Char]]
+split [] = []
+split (' ':[]) = []
+split (' ':' ':xs) = split(xs)
+split (' ':'-':xs) = split(xs)
+split ('-':' ':xs) = split(xs)
+split ('-':'-':xs) = split(xs)
+split (' ':xs) = [wd(xs)] ++ split(xs)
+split ('-':[]) = []
+split ('-':xs) = [wd(xs)] ++ split(xs)
+split (x:xs) = [] ++ split(xs)
+
+wd :: [Char] -> [Char]
+wd [] = []
+wd (' ':[]) = []
+wd (' ':xs) = []
+wd (x:[]) = [x]
+wd (x:xs) = [x] ++ wd(xs)
