cceb4adf6ee7193a980cf1270af314017cffd67a
mikedamay <mikemay@blueyonder.co.uk>
2019-05-02 12:13:20 +0100

    haskell/anagram - 2nd draft

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -4,7 +4,11 @@ import Data.List (sort)
 import Data.Char (toLower)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = [ x | x <- (excludeWord xs xss), (sort (map toLower x)) == (sort (map toLower xs))]
+anagramsFor xs xss =
+    let
+        lwrWord = map toLower xs
+    in
+    [ x | x <- (excludeWord lwrWord xss), (sort (map toLower x)) == (sort lwrWord)]
 
 excludeWord :: String -> [String]  -> [String]
 excludeWord xs xss = [x | x <- xss, (map toLower x) /= (map toLower xs) ]
