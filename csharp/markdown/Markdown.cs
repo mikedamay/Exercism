f0eda61d9ddac85785815cb88074f1e7267cef4f
mikedamay <mikemay@blueyonder.co.uk>
2019-05-10 06:10:04 +0100

    csharp/markdown - tweak

--- csharp/markdown/Markdown.cs
+++ csharp/markdown/Markdown.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using Xunit;
 
 
 internal enum TokenType /* or stateType */ { Root, Para, Strong, Italic, Text, Newline
