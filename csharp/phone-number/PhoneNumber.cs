0f4d2cc157d053c293b6296eb86fe4cbbdfadadd
mikedamay <mikemay@blueyonder.co.uk>
2019-05-18 08:10:15 +0100

    csharp/phone-number - 1st draft

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Linq;
 
 public class PhoneNumber
 {
     public static string Clean(string phoneNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var str = string.Concat(phoneNumber
+            .Where(Char.IsDigit)
+            .Where((c, idx) => !(idx == 0 && c == '1'))
+            .Select((c, idx) => (idx == 0 || idx == 3)  && (c == '0' || c == '1') ? throw new ArgumentException() : c )
+            .Select(c => !Char.IsDigit(c) ? throw new ArgumentException() : c )
+        );
+        return str.Length == 10 ? str : throw new ArgumentException();
     }
 }
