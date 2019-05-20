f6014189486f4844a281db7cf42b751498195e38
mikedamay <mikemay@blueyonder.co.uk>
2019-05-20 14:01:57 +0100

    haskell/crypto-square - backup beginning to take shape

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -9,21 +9,22 @@ dims :: String -> (Int, Int)
 dims s = ((ceiling len), (floor len))
     where len = sqrt (fromIntegral (length s))
 
-stripper :: String -> [[(Int, Int, Char)]]
-stripper xs = strip xs 2 2 0
+stripper :: String -> [(Int, Int, Char)]
+stripper xs = sortBy getKey (concat $ strip xs numCols 0 0)
+    where (numCols, numRows) = dims xs
 
 strip :: String -> Int -> Int -> Int -> [[(Int, Int, Char)]]
 strip [] _ _ _ = []
 strip (x:xs) cols col row
     | col == cols = strip (x:xs) cols 0 (row + 1)
-    | col == 0 = [sortBy getKey (grab (x:xs) cols cols row)] ++ (strip xs cols (col + 1) row)
+    | col == 0 = [(grab (x:xs) cols 0 row)] ++ (strip xs cols (col + 1) row)
     | otherwise = strip xs cols (col + 1) row
 
 grab :: String -> Int -> Int -> Int -> [(Int, Int, Char)]
 grab [] _ _ _ = []
 grab (x:xs) numCols col row
-    | col == 0 = []
-    | otherwise = [(numCols, col, x)] ++ (grab xs numCols (col - 1) row)
+    | col == numCols = []
+    | otherwise = [(row, col, x)] ++ (grab xs numCols (col + 1) row)
 
 getKey :: (Int, Int, Char) -> (Int, Int, Char) -> Ordering
 getKey (row, col, ch) (orow, ocol, och) = compare (col, row) (ocol, orow)
