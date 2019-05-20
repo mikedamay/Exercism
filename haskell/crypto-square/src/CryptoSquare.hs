ec72466e68d33763853b7c7acd0b71e3d8893775
mikedamay <mikemay@blueyonder.co.uk>
2019-05-20 15:20:39 +0100

    haskell/crypto-square - all tests pass

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -1,17 +1,22 @@
-module CryptoSquare (encode, dims, strip) where
+module CryptoSquare (encode, dims, strip, clean, padTo, getKey, fmt) where
 
 import Data.List (sortBy)
+import Data.Char (toLower, isLetter, isAlphaNum)
 
 encode :: String -> String
-encode xs = show (length xs) 
+encode xs = (stripper xs)
 
 dims :: String -> (Int, Int)
-dims s = ((ceiling len), (floor len))
+dims s = ((ceiling len), (ceiling len))
     where len = sqrt (fromIntegral (length s))
 
-stripper :: String -> [(Int, Int, Char)]
-stripper xs = sortBy getKey (concat $ strip xs numCols 0 0)
-    where (numCols, numRows) = dims xs
+stripper :: String -> [Char]
+-- stripper :: String -> [(Int, Int, Char)]
+stripper xs = fmt (sortBy getKey (concat $ strip rectStr numCols 0 0)) numCols numCols
+    where
+        rectStr = padTo rectSize (clean xs)
+        rectSize = numCols * numRows
+        (numCols, numRows) = dims (clean xs)
 
 strip :: String -> Int -> Int -> Int -> [[(Int, Int, Char)]]
 strip [] _ _ _ = []
@@ -26,8 +31,23 @@ grab (x:xs) numCols col row
     | col == numCols = []
     | otherwise = [(row, col, x)] ++ (grab xs numCols (col + 1) row)
 
+fmt :: [(Int, Int, Char)] -> Int -> Int -> String
+fmt [] _ _ = []
+fmt (x:xs) chunkSize ctr
+    | ctr == 0 = ' ' : (fmt (x:xs) chunkSize chunkSize)
+    | otherwise = (extract x) : (fmt xs chunkSize (ctr - 1))
+
+extract :: (Int, Int, Char) -> Char
+extract (r,c,s) = s
+
 getKey :: (Int, Int, Char) -> (Int, Int, Char) -> Ordering
 getKey (row, col, ch) (orow, ocol, och) = compare (col, row) (ocol, orow)
+
+padTo :: Int -> String -> String
+padTo n str = str ++ (replicate (n - (length str)) ' ')
+
+clean :: String -> String
+clean xs = [ toLower x | x <- xs, isAlphaNum x]
 
 {-
 tt :: String -> [String]
@@ -39,4 +59,4 @@ tt (x:xs) = [[x]] ++ [(grab xs 2 2)]
 -- round up for cols, round down for rows
 -- pad with spaces
 -- build list of strings
--- 
+--
