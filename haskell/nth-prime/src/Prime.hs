ae9069f6dce20288c68803ff4a007ac7e707357d
mikedamay <mikemay@blueyonder.co.uk>
2019-05-23 11:02:21 +0100

    haskell/nth-prime - complete

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,28 +1,12 @@
 module Prime (nth) where
 
-nth :: Int -> [Integer]
-nth n = sieve [2..] n
+nth :: Int -> Maybe Integer
+nth 0 = Nothing
+nth n = Just (sieve [2..] (n - 1))
 
-sieve :: [Integer] -> Int -> [Integer]
+sieve :: [Integer] -> Int -> Integer
+sieve [] _ = 0 -- avoid warning
 sieve (cand:candidates) ctr
-    | ctr == 0 = []
-    | otherwise = cand : (sieve [x | x <- candidates, x `mod` cand /= 0] (ctr - 1))
+    | ctr == 0 = cand
+    | otherwise = sieve [x | x <- candidates, x `mod` cand /= 0] (ctr - 1)
 
-{-
-nextPrime :: ([Integer], Integer) -> ([Integer], Integer)
-nextPrime ([], ctr) = ([], ctr)
-nextPrime ((cand:candidates), ctr) = (cand : (nextPrime [x | x <- candidates, x `mod` cand /= 0]), ctr + 1)
-
-
-sieve :: Integer -> [Integer] -> Integer -> [Integer]
-sieve n candidates ctr
-    | ctr >= n = []
-    | otherwise = sieved ++ (sieve n cands ctr')
-        where cands = nextCandidates candidates
-              (sieved, ctr') = nextPrime cands ctr
-
-nextCandidates :: [Integer] -> [Integer]
-nextCandidates candidates = [start..end]
-    where
-        start =
--}
