edae28c7549abdf5c07c5840a1183289afdfbafc
mikedamay <mikemay@blueyonder.co.uk>
2019-05-23 10:45:28 +0100

    haskell/nth-prime - prime counting working well

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,10 +1,28 @@
 module Prime (nth) where
 
 nth :: Int -> [Integer]
-nth n = nextPrime (drop 1  [1..100])
+nth n = sieve [2..] n
 
-nextPrime :: [Integer] -> [Integer]
-nextPrime [] = []
-nextPrime (cand:candidates) = cand : (nextPrime [x | x <- candidates, x `mod` cand /= 0])
+sieve :: [Integer] -> Int -> [Integer]
+sieve (cand:candidates) ctr
+    | ctr == 0 = []
+    | otherwise = cand : (sieve [x | x <- candidates, x `mod` cand /= 0] (ctr - 1))
+
+{-
+nextPrime :: ([Integer], Integer) -> ([Integer], Integer)
+nextPrime ([], ctr) = ([], ctr)
+nextPrime ((cand:candidates), ctr) = (cand : (nextPrime [x | x <- candidates, x `mod` cand /= 0]), ctr + 1)
 
 
+sieve :: Integer -> [Integer] -> Integer -> [Integer]
+sieve n candidates ctr
+    | ctr >= n = []
+    | otherwise = sieved ++ (sieve n cands ctr')
+        where cands = nextCandidates candidates
+              (sieved, ctr') = nextPrime cands ctr
+
+nextCandidates :: [Integer] -> [Integer]
+nextCandidates candidates = [start..end]
+    where
+        start =
+-}
