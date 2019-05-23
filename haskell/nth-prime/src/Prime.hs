4d34001432e81787055c4925320ca16bf24c9877
mikedamay <mikemay@blueyonder.co.uk>
2019-05-23 09:22:13 +0100

    haskell/nth-prime - eratosthenes sieve - final

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,10 +1,10 @@
 module Prime (nth) where
 
 nth :: Int -> [Integer]
-nth n = snd (nextPrime ((drop 1  [1..100]), []))
+nth n = nextPrime (drop 1  [1..100])
 
-nextPrime :: ([Integer], [Integer]) -> ([Integer], [Integer])
-nextPrime ([], sieved) = ([], sieved)
-nextPrime (cand:candidates, sieved) = nextPrime ([x | x <- candidates, x `mod` cand /= 0], cand : sieved)
+nextPrime :: [Integer] -> [Integer]
+nextPrime [] = []
+nextPrime (cand:candidates) = cand : (nextPrime [x | x <- candidates, x `mod` cand /= 0])
 
 
