99bdbc009df14686ef5c43b7135bacbf251bca1f
mikedamay <mikemay@blueyonder.co.uk>
2019-05-23 09:14:24 +0100

    haskell/nth-prime - eratosthenes sieve - ist draft

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,4 +1,10 @@
 module Prime (nth) where
 
-nth :: Int -> Maybe Integer
-nth n = error "You need to implement this function."
+nth :: Int -> [Integer]
+nth n = snd (nextPrime ((drop 1  [1..100]), []))
+
+nextPrime :: ([Integer], [Integer]) -> ([Integer], [Integer])
+nextPrime ([], sieved) = ([], sieved)
+nextPrime (cand:candidates, sieved) = nextPrime ([x | x <- candidates, x `mod` cand /= 0], cand : sieved)
+
+
