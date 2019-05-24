48220c4677eef515d352a50698f4f0a275a36aee
mikedamay <mikemay@blueyonder.co.uk>
2019-05-24 14:42:26 +0100

    haskell/luhn - 1st draft

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -1,4 +1,30 @@
-module Luhn (isValid) where
+module Luhn (isValid, towardsLuhn, luhnify, removeEveryOther, twiddle) where
+
+import Data.Char (isDigit, digitToInt)
 
 isValid :: String -> Bool
-isValid n = error "You need to implement this function."
+isValid n = (sum $ towardsLuhn n) `mod` 10 == 0
+
+towardsLuhn :: String -> [Integer]
+towardsLuhn numStr = removeEveryOther cleaned
+    where cleaned = [c2 | c2 <- numStr, isDigit c2 ]
+
+luhnify :: Char -> [Integer]
+luhnify c =
+    let n = toInteger $ digitToInt c in
+    [n, ((n*2) `mod` 10)]
+
+
+removeEveryOther :: String -> [Integer]
+removeEveryOther a = removeEveryOther' (length a) a
+
+removeEveryOther' :: Int -> String -> [Integer]
+removeEveryOther' _ [] = []
+removeEveryOther' ctr (x:xs)
+    | ctr `mod` 2 == 0 = (twiddle n) : (removeEveryOther' (ctr - 1) xs)
+    | otherwise        = n : removeEveryOther' (ctr - 1) xs
+    where n = toInteger $ digitToInt x
+
+twiddle :: Integer -> Integer
+twiddle n = if dub > 9 then dub - 9 else dub
+    where dub = n * 2
