044576395d72112f29aa606162175c760d2d1345
mikedamay <mikemay@blueyonder.co.uk>
2019-05-25 08:40:42 +0100

    haskell/luhn - cleaned up

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -1,4 +1,4 @@
-module Luhn (isValid, towardsLuhn, luhnify, removeEveryOther, twiddle) where
+module Luhn (isValid, towardsLuhn, luhnify) where
 
 import Data.Char (isDigit, digitToInt)
 
@@ -6,25 +6,19 @@ isValid :: String -> Bool
 isValid n = (sum $ towardsLuhn n) `mod` 10 == 0
 
 towardsLuhn :: String -> [Integer]
-towardsLuhn numStr = removeEveryOther cleaned
+towardsLuhn numStr = convertEveryOtherDigit cleaned
     where cleaned = [c2 | c2 <- numStr, isDigit c2 ]
 
-luhnify :: Char -> [Integer]
-luhnify c =
-    let n = toInteger $ digitToInt c in
-    [n, ((n*2) `mod` 10)]
+convertEveryOtherDigit :: String -> [Integer]
+convertEveryOtherDigit a = convertEveryOtherDigit' (length a) a
 
-
-removeEveryOther :: String -> [Integer]
-removeEveryOther a = removeEveryOther' (length a) a
-
-removeEveryOther' :: Int -> String -> [Integer]
-removeEveryOther' _ [] = []
-removeEveryOther' ctr (x:xs)
-    | ctr `mod` 2 == 0 = (twiddle n) : (removeEveryOther' (ctr - 1) xs)
-    | otherwise        = n : removeEveryOther' (ctr - 1) xs
+convertEveryOtherDigit' :: Int -> String -> [Integer]
+convertEveryOtherDigit' _ [] = []
+convertEveryOtherDigit' ctr (x:xs)
+    | ctr `mod` 2 == 0 = (makeLuhnDigit n) : (convertEveryOtherDigit' (ctr - 1) xs)
+    | otherwise        = n : convertEveryOtherDigit' (ctr - 1) xs
     where n = toInteger $ digitToInt x
 
-twiddle :: Integer -> Integer
-twiddle n = if dub > 9 then dub - 9 else dub
+makeLuhnDigit :: Integer -> Integer
+makeLuhnDigit n = if dub > 9 then dub - 9 else dub
     where dub = n * 2
