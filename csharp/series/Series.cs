abd0ff389c67168a67e7234f00662cdd895ee767
mikedamay <mikemay@blueyonder.co.uk>
2019-05-26 08:07:40 +0100

    csharp/series - for publication

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Linq;
 
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        bool isInvalid() => numbers.Length == 0
+                          || sliceLength <= 0
+                          || sliceLength > numbers.Length;
+        return isInvalid() 
+            ? throw new ArgumentException()
+            : Enumerable.Range(0, numbers.Length - sliceLength + 1)
+                        .Select(n => numbers.Substring(n, sliceLength))
+                        .ToArray();
     }
 }
