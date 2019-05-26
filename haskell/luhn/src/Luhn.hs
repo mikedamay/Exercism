a6c635e21a00f52db927fe31350157cbed4d8af4
mikedamay <mikemay@blueyonder.co.uk>
2019-05-26 07:14:28 +0100

    haskell/luhn - fixed code failing tests

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -4,12 +4,13 @@ import Data.Char (isDigit, digitToInt, isSpace)
 
 isValid :: String -> Bool
 isValid n
-    | removeSpaces n == "0" = False
+    | (length (removeSpaces n)) <= 1 = False
     | otherwise = (sum $ towardsLuhn n) `mod` 10 == 0
 
 towardsLuhn :: String -> [Integer]
-towardsLuhn numStr = [ luhnify x | x <- (zip (map (toInteger . digitToInt) cleaned) [0..])]
+towardsLuhn numStr = [ luhnify x | x <- (zip (map (toInteger . digitToInt) cleaned) [len_cleaned, (len_cleaned - 1)..0])]
     where cleaned = [c2 | c2 <- numStr, isDigit c2 ]
+          len_cleaned = toInteger (length cleaned)
 
 luhnify :: (Integer, Integer) -> Integer
 luhnify (val, idx)
