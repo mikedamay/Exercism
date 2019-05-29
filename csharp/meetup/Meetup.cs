7807c05c4b10698b68854351de577554c6341aa0
mikedamay <mikemay@blueyonder.co.uk>
2019-05-29 09:08:29 +0100

    csharp/series - 1st draft

--- csharp/meetup/Meetup.cs
+++ csharp/meetup/Meetup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public enum Schedule
 {
@@ -12,12 +14,48 @@ public enum Schedule
 
 public class Meetup
 {
+
+    private readonly int month;
+    private readonly int year;
+    private static readonly TimeSpan OneDay = new TimeSpan(1, 0, 0,0 );
     public Meetup(int month, int year)
     {
+        this.month = month;
+        this.year = year;
     }
 
     public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        IEnumerable<DateTime> candidateDays
+            = Days()
+                .Where(d => d.DayOfWeek == dayOfWeek)
+            ;
+        switch (schedule)
+        {
+            case Schedule.First:
+                return candidateDays.First();
+            case Schedule.Last:
+                return candidateDays.Last();
+            case Schedule.Second:
+                return candidateDays.Skip(1).First();
+            case Schedule.Third:
+                return candidateDays.Skip(2).First();
+            case Schedule.Fourth:
+                return candidateDays.Skip(3).First();
+            case Schedule.Teenth:
+                return candidateDays.First(d => d.Day >= 13 && d.Day <= 19);
+            default:
+                throw new ArgumentException();
+        }
+    }
+
+    private IEnumerable<DateTime> Days()
+    {
+        for (DateTime dt = new DateTime(year, month, 1)
+            ; dt.Month == month
+            ; dt += OneDay)
+        {
+            yield return dt;
+        }
     }
 }
