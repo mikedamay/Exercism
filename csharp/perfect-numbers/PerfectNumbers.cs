87200068041d3dee711e4710a9036ab3713ac13f
mikedamay <mikemay@blueyonder.co.uk>
2019-05-29 14:56:02 +0100

    haskell/grade-school - initial download

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -9,8 +9,22 @@ public enum Classification
 
 public static class PerfectNumbers
 {
+    public const int Mike = 42;
     public static Classification Classify(int number)
     {
         throw new NotImplementedException();
+    }
+}
+
+
+namespace Bob
+{
+    public static class PerfectNumbers
+    {
+        public const int John = global::PerfectNumbers.Mike;
+        public static void Foo()
+        {
+        
+        }
     }
 }
