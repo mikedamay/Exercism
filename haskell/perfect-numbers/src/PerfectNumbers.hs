11d41b985fcca744edcacea13583177f21f1204a
mikedamay <mikemay@blueyonder.co.uk>
2019-05-29 14:52:24 +0100

    haskell/perfect-numbers - using . syntax - 1 warning

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -11,6 +11,6 @@ classify n
     | otherwise = Just Deficient
          where
          includeFactor idx' x' n' = if idx' `mod` 2 == 0 then x' else if n' `div` x' == x' then 0 else n' `div` x'
-         maxTrial n' = (floor (sqrt ((fromIntegral n') :: Double)))
+         maxTrial = floor . sqrt . fromIntegral
          result = 1 + (sum [includeFactor idx x n | x <- [2..(maxTrial n)], idx <- [0 :: Integer ..1], n `mod` x == 0])
 
