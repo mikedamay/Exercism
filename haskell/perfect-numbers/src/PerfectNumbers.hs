a3402aebd9f6e7e420cce4527ecd6724bd13c42a
mikedamay <mikemay@blueyonder.co.uk>
2019-05-29 12:40:04 +0100

    haskell/perfect-numbers - 1st draft

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -3,4 +3,13 @@ module PerfectNumbers (classify, Classification(..)) where
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify = error "You need to implement this function."
+classify n
+    | n <= 0 = Nothing
+    | otherwise = if result == n then Just Perfect
+         else if result > n then Just Abundant
+         else Just Deficient
+        where result = sum [x | x <- [1..n`div`2], n `mod` x == 0]
+
+
+-- classify :: Int -> Maybe Classification
+-- classify n = if (floor (sqrt (fromIntegral n ))) > 10 then Nothing else Nothing
