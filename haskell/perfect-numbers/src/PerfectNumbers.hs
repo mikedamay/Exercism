6fce5f1ace7c13ec39b4fbd7e132ff2156b609a8
mikedamay <mikemay@blueyonder.co.uk>
2019-05-29 13:15:10 +0100

    haskell/perfect-numbers - for publication

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -9,5 +9,8 @@ classify n
     | result == n = Just Perfect
     | result > n = Just Abundant
     | otherwise = Just Deficient
-         where result = 1 + (sum [if idx `mod` 2 == 0 then x else if n `div` x == x then 0 else n `div` x | x <- [2..(fromIntegral (floor (sqrt (fromIntegral n))))], idx <- [0..1], n `mod` x == 0])
+         where
+         includeFactor idx' x' n' = if idx' `mod` 2 == 0 then x' else if n' `div` x' == x' then 0 else n' `div` x'
+         maxTrial n' = (floor (sqrt ((fromIntegral n') :: Double)))
+         result = 1 + (sum [includeFactor idx x n | x <- [2..(maxTrial n)], idx <- [0 :: Integer ..1], n `mod` x == 0])
 
