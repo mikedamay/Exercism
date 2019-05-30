08fea139a6f305d9faf94bb2e3f399e97c28fb7a
mikedamay <mikemay@blueyonder.co.uk>
2019-05-30 10:12:28 +0100

    haskell/grade-school - builds with Map - not tested

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -1,22 +1,31 @@
-module School (School, add, empty, grade, sorted, mysort) where
+module School (School, add, empty, grade, sorted) where
 
-import Data.List (nub, sortBy)
+import Data.List (nub, sort, sortBy)
+import qualified Data.Map as Map
 
 data Entry = Entry {gradenum :: Int, student :: String} deriving (Eq, Ord, Show)
 
-type School = [Entry]
+type School = Map.Map Int [String]
 
 add :: Int -> String -> School -> School
-add gn st school = Entry {gradenum = gn, student = st} : school
+-- add gn st school = Map.fromList ((gn,st) : [(1, "abc")])
+add gn st school = Map.fromList (merge (gn,st) (Map.toList school))
+
+merge :: (Int, String) -> [(Int, [String])] -> [(Int, [String])]
+merge y [] = [((fst y), [(snd y)])] :: [(Int, [String])]
+merge y (x:xs)
+    | (fst y) == (fst x) = ((fst x), ((snd y):(snd x))) : (merge y xs)
+    | otherwise = (x:(merge y xs))
 
 empty :: School
-empty = []
+empty = Map.empty
 
 grade :: Int -> School -> [String]
-grade gn school = [(student x) | x <- (mysort [x | x <- school, (gradenum x) == gn])]
+-- grade gn school =  sort [snd x | x <- (Map.toList school), (fst x) == gn]
+grade gn school = sort (snd (head (filter (\x -> (fst x) == gn) (Map.toList school))))
 
 sorted :: School -> [(Int, [String])]
-sorted school = [(g, [(student e) | e <- sorted_school, (gradenum e) == g])  | g <- grades]
-    where sorted_school = sortBy (\x y -> compare ((gradenum x), (student x)) ((gradenum y), (student y))) [x | x <- school]
-          grades = nub [(gradenum x) | x <- sorted_school]
+sorted school = sorted_school
+    where sorted_school = sortBy (\x y -> compare (fst x) (fst y)) [x | x <- (Map.toList school)]
+
 
