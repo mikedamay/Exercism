3ca4566d8efd795fd06e5807275c5a42838276bf
mikedamay <mikemay@blueyonder.co.uk>
2019-05-30 08:53:06 +0100

    haskell/grade-school - use standard sortBy

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -1,6 +1,6 @@
 module School (School, add, empty, grade, sorted, mysort) where
 
-import Data.List (nub)
+import Data.List (nub, sortBy)
 
 data Entry = Entry {gradenum :: Int, student :: String} deriving (Eq, Ord, Show)
 
@@ -17,12 +17,6 @@ grade gn school = [(student x) | x <- (mysort [x | x <- school, (gradenum x) == 
 
 sorted :: School -> [(Int, [String])]
 sorted school = [(g, [(student e) | e <- sorted_school, (gradenum e) == g])  | g <- grades]
-    where sorted_school = mysort [x | x <- school]
+    where sorted_school = sortBy (\x y -> compare ((gradenum x), (student x)) ((gradenum y), (student y))) [x | x <- school]
           grades = nub [(gradenum x) | x <- sorted_school]
 
-mysort :: [Entry] -> [Entry]
-mysort [] = []
-mysort (x:xs) =
-    mysort [x2 | x2 <- xs, (lesser x2 x)] ++ [x] ++ [x2 | x2 <- xs, (greater x2 x)]
-    where lesser cand pivot = (gradenum cand) < (gradenum pivot) || (gradenum cand) == (gradenum pivot) && (student cand) < (student pivot)
-          greater cand pivot = (gradenum cand) > (gradenum pivot) || (gradenum cand) == (gradenum pivot) && (student cand) >= (student pivot)
