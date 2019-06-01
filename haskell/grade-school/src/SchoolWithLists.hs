dab24dd938e8bcf83375782e5866304da74d6b05
mikedamay <mikemay@blueyonder.co.uk>
2019-06-01 06:24:50 +0100

    haskell/grade-school - school with lists passes tests

---
+++ haskell/grade-school/src/SchoolWithLists.hs
@@ -0,0 +1,23 @@
+module SchoolWithLists (School, add, empty, grade, sorted) where
+
+import Data.List (nub, sortBy)
+
+data Entry = Entry {gradenum :: Int, student :: String} deriving (Eq, Ord, Show)
+
+type School = [Entry]
+
+add :: Int -> String -> School -> School
+add gn st school = Entry {gradenum = gn, student = st} : school
+
+empty :: School
+empty = []
+
+grade :: Int -> School -> [String]
+grade gn school = [(student x) | x <- (sortBy (\x y -> compare ((gradenum x), (student x)) ((gradenum y), (student y))) [x | x <- school, (gradenum x) == gn])]
+
+sorted :: School -> [(Int, [String])]
+sorted school = [(g, [(student e) | e <- sorted_school, (gradenum e) == g])  | g <- grades]
+    where sorted_school = sortBy (\x y -> compare ((gradenum x), (student x)) ((gradenum y), (student y))) [x | x <- school]
+          grades = nub [(gradenum x) | x <- sorted_school]
+
+
