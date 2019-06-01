7838664496bb99801e047d1348e5d167e28c1a0f
mikedamay <mikemay@blueyonder.co.uk>
2019-06-01 07:36:45 +0100

    haskell/grade-school - school with map (using fromList

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -1,6 +1,6 @@
 module School (School, add, empty, grade, sorted, merge) where
 
-import Data.List (nub, sort, sortBy)
+import Data.List (sort, sortBy)
 import qualified Data.Map as Map
 
 data Entry = Entry {gradenum :: Int, student :: String} deriving (Eq, Ord, Show)
@@ -14,20 +14,22 @@ add gn st school
     where school_list = Map.toList school
 
 contains :: [(Int,[String])] -> Int-> Bool
-contains [] gn = False
+contains [] _ = False
 contains (x:xs) gn = (gn == (fst x)) || (contains xs gn)
 
 merge :: (Int, String) -> [(Int, [String])] -> [(Int, [String])]
-merge y [] = [((fst y), [(snd y)])] :: [(Int, [String])]
+merge _ [] = []
 merge y (x:xs)
-    | (fst y) == (fst x) = ((fst x), ((snd y):(snd x))) : (merge y xs)
+    | (fst y) == (fst x) = [((fst x), sort ((snd y):(snd x)))] ++ xs
     | otherwise = (x:(merge y xs))
 
 empty :: School
 empty = Map.empty
 
 grade :: Int -> School -> [String]
-grade gn school = sort (snd (head (filter (\x -> (fst x) == gn) (Map.toList school))))
+grade gn school
+    | Map.lookup gn school == Nothing = []
+    | otherwise = sort (snd (head (filter (\x -> (fst x) == gn) (Map.toList school))))
 
 sorted :: School -> [(Int, [String])]
 sorted school = sorted_school
