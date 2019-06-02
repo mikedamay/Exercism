94812704af6789819c598e553945d0880e170b8c
mikedamay <mikemay@blueyonder.co.uk>
2019-06-02 07:30:24 +0100

    csharp/pythagorean-triplet - 1st draft - non-performant

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -5,6 +5,21 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        for (int aa = 3; aa <= sum; aa++)
+        {
+            for (int bb = aa + 1; bb <= sum - aa; bb++)
+            {
+                if (aa * bb % 12 != 0)
+                    continue;
+                for (int cc = bb + 1; cc <= sum - aa - bb; cc++)
+                {
+                    if (aa + bb + cc == sum &&
+                        
+                        aa < bb && bb < cc 
+                                && aa * aa + bb * bb == cc * cc)
+                        yield return (aa, bb, cc);
+                }
+            }
+        }
     }
 }
