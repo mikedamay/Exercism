a5a0dd6851c160094068dd7b17d00fb272df2bd9
mikedamay <mikemay@blueyonder.co.uk>
2019-06-03 13:29:38 +0100

    haskell/diamond - backup

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,24 +1,16 @@
 ﻿using System;
-using System.Linq;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        return !word.Select(c => Char.ToLower(c))
-            .Where(c => Char.IsLetter(c))
-            .OrderBy(c => c)
-            .GroupBy(c => c)
-            .Any(g => g.Count() > 1);
+        string loweredWord = word.ToLower();
+        int[] count = new int[256];
+        foreach(char c in word)
+        {
+            if (++count[c] > 1)
+                return false;
+        }
+        return true;
     }
 }
-
-/*
-Review Points:
-
-Good solution
-
-Discussion Points:
-
-You might want to think about a LINQ based solution (or you might not see the point in which case let me know).  Use of `Distinct()` or `GroupBy()` indicate two different ways to approach this.
-*/
--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -1,4 +1,19 @@
-module Diamond (diamond) where
+module Diamond (diamond, generateLine, numCols, letterIndex) where
 
 diamond :: Char -> Maybe [String]
-diamond = error "You need to implement this function"
+diamond ch = Nothing
+
+generateLine :: Char -> Char -> String
+generateLine targetCh = generateLine' targetCh
+generateLine' targetCh ch = padLeft ++ [ch] ++ padMid ++ secondCh ++ padRight
+    where padLeft = take (((numCols targetCh) `div` 2) - offset) (repeat ' ')
+          padMid = take ((numCols targetCh) - ((length padLeft) * 2) - 2) (repeat ' ')
+          padRight = padLeft
+          offset = letterIndex ch
+          secondCh = if ch == 'A' then [] else [ch]
+
+numCols :: Char -> Int
+numCols ch = (+) 1 $ (*) 2 $ letterIndex ch
+
+letterIndex :: Char -> Int
+letterIndex ch = (fromEnum ch) - (fromEnum 'A')
