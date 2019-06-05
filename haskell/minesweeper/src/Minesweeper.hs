0f9fcf2f95c0af791ccad4b52011c9d2eab6c76c
mikedamay <mikemay@blueyonder.co.uk>
2019-06-05 10:01:57 +0100

    haskell/minesweeper - still more refinement - all tests still passing

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -2,6 +2,11 @@ module Minesweeper (annotate) where
 
 import Data.Char (intToDigit)
 
+type Row = Int
+type Col = Int
+type Val = Int
+
+
 annotate :: [String] -> [String]
 annotate [""] = [""]
 annotate board = cellsToText $ sweep $ textToCells board
@@ -9,23 +14,25 @@ annotate board = cellsToText $ sweep $ textToCells board
           cellListsToText = reverse . (map convertListToString)
           boardToMultipleLists = splitList [] areDifferentRows False
 
-sweep :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+sweep :: [(Row, Col, Val)] -> [(Row, Col, Val)]
 sweep cells = (onList [] sumSubLists) $ map (detectMine cells) cells
 
-textToCells :: [String] -> [(Int, Int, Int)]
+textToCells :: [String] -> [(Row, Col, Val)]
 textToCells [] = []
-textToCells b = concat $ map addCoordsToLine $ zip b [0 :: Int ..]
+textToCells b = concat $ map lineToCells $ zip b [0 :: Int ..]
 
-addCoordsToLine :: ([Char], Int) -> [(Int, Int, Int)]
-addCoordsToLine (line,row) = map (\x -> (row, snd x, if (fst x) == '*' then -1 else 0) ) $ zip line [0..]
+lineToCells :: ([Char], Int) -> [(Row, Col, Val)]
+lineToCells (line,row) = map (\x -> (row, snd x, if (fst x) == '*' then -1 else 0) ) $ zip line [0..]
 
-detectMine :: [(Int, Int, Int)] -> (Int, Int, Int) -> [(Int, Int, Int)]
-detectMine positions cell = map (addAdjacent cell) positions
+-- detectMine examines each cell together with every cell on the board to find out if they are neighbours
+-- candidateNeighbours - every cell on the board.
+detectMine :: [(Row, Col, Val)] -> (Row, Col, Val) -> [(Row, Col, Val)]
+detectMine candidateNeighbours cell = map (addAdjacent cell) candidateNeighbours
 
-addAdjacent :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
+addAdjacent :: (Row, Col, Val) -> (Row, Col, Val) -> (Row, Col, Val)
 addAdjacent cell candidate@(row, col, val) = if val == -1 then (row, col, -1) else if isNeighbour cell candidate then (row, col, 1) else (row, col, 0)
 
-isNeighbour :: (Int, Int, Int) -> (Int, Int, Int) -> Bool
+isNeighbour :: (Row, Col, Val) -> (Row, Col, Val) -> Bool
 isNeighbour (row, col, val) (candRow, candCol, candVal) =
     val == -1 && (candRow /= row || candCol /= col) && candVal /= -1 && abs(candRow - row) <= 1 && abs(candCol - col) <= 1
 
@@ -38,7 +45,7 @@ splitList (z:zs) f True (x:y:xs) = splitList ([x]:z:zs) f (f x y) (y:xs)
 splitList (z:zs) _ False (x:[]) = (x:z):zs
 splitList (z:zs) _ True (x:[]) = [x]:z:zs
 
-areDifferentRows :: (Int, Int, Int) -> (Int, Int, Int) -> Bool
+areDifferentRows :: (Row, Col, Val) -> (Row, Col, Val) -> Bool
 areDifferentRows (a, _, _) (b, _, _) = a /= b
 
 onList :: [a] -> ([a] -> [a] -> [a]) -> [[a]] -> [a]
@@ -46,30 +53,20 @@ onList zs _ [] = zs
 onList [] f (x:xs) = onList x f xs
 onList zs f (x:xs) = onList (f x zs) f xs
 
-sumSubLists :: [(Int, Int, Int)] -> [(Int, Int, Int)] -> [(Int, Int, Int)]
+sumSubLists :: [(Row, Col, Val)] -> [(Row, Col, Val)] -> [(Row, Col, Val)]
 sumSubLists = zipWith mergeCellInstanes
 -- sumSubLists x y = [((first (fst p)), (second (fst p)), (third (fst p)) + (third (snd p))) | p <- (zip x y)]
 
-mergeCellInstanes :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
+mergeCellInstanes :: (Row, Col, Val) -> (Row, Col, Val) -> (Row, Col, Val)
 mergeCellInstanes (xrow, xcol, xval) (_, _, yval) = (xrow, xcol, xval + yval)
 
 
-mineLocationToText :: (Int, Int, Int) -> Char
-mineLocationToText (_, _, val)
+cellToChar :: (Row, Col, Val) -> Char
+cellToChar (_, _, val)
     | val < 0 = '*'
     | val == 0 = ' '
     | otherwise = intToDigit val
 
-convertListToString :: [(Int, Int, Int)] -> String
-convertListToString = reverse . map mineLocationToText
+convertListToString :: [(Row, Col, Val)] -> String
+convertListToString = reverse . map cellToChar
 
--- locateMines :: [(Int, Int, Int)] -> [[(Int,Int,Int)]]
--- locateMines cells = [(first x, second x, valForXY x y) | x <- cells, y <- cells]
---
--- isNeighbourOf :: (Int, Int, Int) -> (Int, Int, Int) -> Bool
--- isNeighbourOf = isNeighbour
---
--- valForXY :: (Int, Int, Int) -> (Int, Int, Int) -> Int
--- valForXY x y
---     | x `isNeighbourOf` y = 1
---     | otherwise = 0
