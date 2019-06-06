314bff746071d7d4707455d4fb5999aef4d8d4f4
mikedamay <mikemay@blueyonder.co.uk>
2019-06-06 15:22:59 +0100

    haskell/phone-number - 1st draft

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,4 +1,13 @@
 module Phone (number) where
 
+import Data.Char (isDigit, digitToInt)
+
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
+number pn
+    | not $ isValid = Nothing
+    | otherwise = Just cleaned
+    where cleaned = queryStrip [x | x <- pn, isDigit x ]
+          queryStrip (x:xs) = if x == '1' then xs else x:xs
+          queryStrip _ = error "in the wrong place"
+          isValid = length cleaned == 10 && digitToInt (cleaned !! 0) >= 2
+                                         && digitToInt (cleaned !! 3) >= 2
