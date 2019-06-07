4b84ca6c783c34655b77d8e5c28fdf4c10100b73
mikedamay <mikemay@blueyonder.co.uk>
2019-06-07 09:07:10 +0100

    haskell/pascals-triangle - for publication

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -1,4 +1,10 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows 0 = []
+rows x = take x ([1] : iterate ((1:) . buildTriangle) [1, 1])
+
+buildTriangle :: [Integer] -> [Integer]
+buildTriangle [] = []       -- avoid warning
+buildTriangle (_:[]) = [1]
+buildTriangle row@(x:y:_) = x+y : buildTriangle (tail row)
