5a779e6482e58e744fa54a610388bae26a86fe4e
mikedamay <mikemay@blueyonder.co.uk>
2019-06-07 11:31:35 +0100

    haskell/word-count - backup - tests not passing

--- haskell/word-count/src/WordCount.hs
+++ haskell/word-count/src/WordCount.hs
@@ -1,4 +1,19 @@
 module WordCount (wordCount) where
 
+import Data.List (sort, foldr1)
+
 wordCount :: String -> [(String, Int)]
-wordCount xs = error "You need to implement this function."
+wordcount "" = []
+wordCount xs = count ((sort . words) xs) []
+
+
+count :: [String] -> [(String, Int)] -> [(String, Int)]
+count [] _ = []
+count (x:[]) [] = [(x, 1)]
+count (x:xs) [] = count xs [(x, 1)]
+count (x:[]) acc@((aw, ac):remainder)
+    | x == aw = (aw, ac + 1):remainder
+    | otherwise = (x, 1):acc
+count (x:xs) acc@((aw, ac):remainder)
+    | x == aw = count xs ((aw, ac + 1):remainder)
+    | otherwise = count xs ((x, 1):acc)
