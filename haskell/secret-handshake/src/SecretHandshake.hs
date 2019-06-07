a8a74983f6530d6c56c73f92d96c2c86d3e841b0
mikedamay <mikemay@blueyonder.co.uk>
2019-06-07 06:33:03 +0100

    haskell/secret-handshake - 1st draft

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -1,4 +1,19 @@
 module SecretHandshake (handshake) where
 
+import Data.Bits
+
 handshake :: Int -> [String]
-handshake n = error "You need to implement this function."
+handshake n = forwardOrBack n $ filter (/="") $ map ($ n) signs
+
+signs :: [(Int -> String)]
+signs = [0x1 `gives` "wink", 0x2 `gives` "double blink", 0x4 `gives` "close your eyes", 0x8 `gives` "jump"]
+
+forwardOrBack :: Int -> ([a] -> [a])
+forwardOrBack n
+    | n .&. 0x10 == 0x10 = reverse
+    | otherwise = id
+
+gives :: Int -> String -> Int -> String
+gives val sign handshake'
+    | val .&. handshake' == val = sign
+    | otherwise = ""
