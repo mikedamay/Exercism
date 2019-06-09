0b3f8b1683bcb1dd8467b75f5cfb7cbadb0b4517
mikedamay <mikemay@blueyonder.co.uk>
2019-06-09 07:00:42 +0100

    haskell/binary-search-tree - no tests attemted
    On hold while I learn about monads

--- haskell/binary-search-tree/src/BST.hs
+++ haskell/binary-search-tree/src/BST.hs
@@ -10,28 +10,32 @@ module BST
     , toList
     ) where
 
-data BST a = Dummy deriving (Eq, Show)
 
-bstLeft :: BST a -> Maybe (BST a)
-bstLeft tree = error "You need to implement this function."
+-- data BST a = BST a (Maybe (BST a)) (Maybe (BST a)) deriving (Eq, Show)
+data BST a = BST {bstValue :: Maybe a, bstLeft :: Maybe (BST a), bstRight :: Maybe (BST a) } | Empty deriving (Eq, Show)
 
-bstRight :: BST a -> Maybe (BST a)
-bstRight tree = error "You need to implement this function."
+-- bstLeft :: BST a -> Maybe (BST a)
+-- bstLeft (BST _ Nothing _) = Nothing
+--
+-- bstRight :: BST a -> Maybe (BST a)
+-- bstRight tree = error "You need to implement this function."
 
-bstValue :: BST a -> Maybe a
-bstValue tree = error "You need to implement this function."
+-- bstValue :: BST a -> Maybe a
+-- bstValue tree = error "You need to implement this function."
 
 empty :: BST a
-empty = error "You need to implement this function."
+empty = Empty
 
 fromList :: Ord a => [a] -> BST a
+fromList [] = Empty
 fromList xs = error "You need to implement this function."
 
 insert :: Ord a => a -> BST a -> BST a
 insert x tree = error "You need to implement this function."
 
 singleton :: a -> BST a
-singleton x = error "You need to implement this function."
+singleton x = BST (Just x) Nothing Nothing
 
 toList :: BST a -> [a]
 toList tree = error "You need to implement this function."
+
