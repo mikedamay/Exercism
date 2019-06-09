0f96b73d32592d41eb39fb388323cafd66c7b7bf
mikedamay <mikemay@blueyonder.co.uk>
2019-06-09 07:09:17 +0100

    haskell/hamming - for publication

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,4 +1,6 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "You need to implement this function."
+distance xs ys
+    | (length xs) /= (length ys) = Nothing
+    | otherwise = Just (sum $ zipWith (\x y -> if x == y then 0 else 1) xs ys)
