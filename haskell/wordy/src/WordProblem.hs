7c5789e4f5a02fcd92b7ca6c152b6c8d221096ae
mikedamay <mikemay@blueyonder.co.uk>
2019-06-09 12:51:19 +0100

    haskell/wordy - substantially complete - not tested

--- haskell/wordy/src/WordProblem.hs
+++ haskell/wordy/src/WordProblem.hs
@@ -1,4 +1,37 @@
 module WordProblem (answer) where
 
+import Data.List (words)
+import Data.Char (isDigit)
+import Text.Read (readMaybe)
+
+data ParsedAs = Operand Integer | BinOp (Integer -> Integer -> Integer) | Separator | Err
+
 answer :: String -> Maybe Integer
-answer problem = error "You need to implement this function."
+answer problem = compute $ map parseToken cleaned
+    where cleaned = drop 2 $ words $ trimEnd problem
+
+compute :: [ParsedAs] -> Maybe Integer
+compute [] = Just 0
+compute ((Operand x):[]) = Just x
+compute (Err:[]) = Nothing
+compute (_:[]) = Nothing
+compute (op1:operator:[]) = Nothing
+compute (op1:operator:op2:xs) = compute ((binop op1 operator op2):xs)
+
+binop :: ParsedAs -> ParsedAs -> ParsedAs -> ParsedAs
+binop (Operand op1) (BinOp operator) (Operand op2) = Operand (operator op1 op2)
+
+parseToken :: String -> ParsedAs
+parseToken x
+    | ((readMaybe x) :: Maybe Int) == Nothing = Operand (read x)
+    | x == "plus" = BinOp (+)
+    | x == "divided" = BinOp div
+    | x == "multiplied" = BinOp (-)
+    | x == "minus" = BinOp (*)
+    | x == "by" = Separator
+    | otherwise = Err
+
+
+
+trimEnd :: String -> String
+trimEnd = foldr (\x acc -> if null acc && not (isDigit x) then [] else x:acc) []
