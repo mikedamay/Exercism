1cc447314a96699a50c5b2eed87b7ae44a3b567f
mikedamay <mikemay@blueyonder.co.uk>
2019-06-12 18:06:55 +0100

    haskell/binary-search-tree - warning free

--- haskell/binary-search-tree/src/BST.hs
+++ haskell/binary-search-tree/src/BST.hs
@@ -10,18 +10,9 @@ module BST
     , toList
     ) where
 
-instance Functor (BST) where
-    fmap f Empty = Empty
-    fmap f t = Node (f (bstValue' t)) (fmap f (bstLeft' t)) (fmap f (bstRight' t))
-
-instance Applicative (BST) where
-    pure a = Node a Empty Empty
-    f <*> t = Node ((bstValue' f) (bstValue' t)) (f <*> (bstLeft' t)) (f <*> (bstRight' t))
-
-
 
 -- data BST a = BST a (Maybe (BST a)) (Maybe (BST a)) deriving (Eq, Show)
-data BST a = Node {bstValue' :: a, bstLeft' :: BST a, bstRight' :: BST a } | Empty deriving (Eq, Show)
+data BST a = Node a (BST a) (BST a) | Empty deriving (Eq, Show)
 
 bstLeft :: BST a -> Maybe (BST a)
 bstLeft Empty = Nothing
@@ -35,7 +26,7 @@ bstRight (Node _ _ r) = Just r
 
 bstValue :: BST a -> Maybe a
 bstValue Empty = Nothing
-bstValue tree = Just (bstValue' tree)
+bstValue (Node a _ _) = Just a
 
 empty :: BST a
 empty = Empty
@@ -46,15 +37,15 @@ fromList xs = foldr (\x tree -> (insert x tree)) Empty (reverse xs)
 
 insert :: Ord a => a -> BST a -> BST a
 insert x Empty = Node x Empty Empty
-insert x tree
---     | x == bstValue' tree = Node x (bstLeft' tree) (bstRight' tree)
-    | x > bstValue' tree = Node (bstValue' tree) (bstLeft' tree) (insert x (bstRight' tree))
-    | x <= bstValue' tree = Node (bstValue' tree) (insert x (bstLeft' tree)) (bstRight' tree)
+insert x (Node a l r)
+    | x > a = Node a l (insert x r)
+    | x <= a = Node a (insert x l) r
+insert _ (Node _ _ _) = error "why does it want this sure insert x (Node a l r) covers it."
 
 singleton :: a -> BST a
 singleton x = Node x Empty Empty
 
 toList :: BST a -> [a]
 toList Empty = []
-toList tree = (toList (bstLeft' tree)) ++ [(bstValue' tree)] ++ (toList (bstRight' tree))
+toList (Node a l r) = (toList l) ++ [a] ++ (toList r)
 
