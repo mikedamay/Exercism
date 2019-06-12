9fbb156ba86544511338f17852b6d28b19756a77
mikedamay <mikemay@blueyonder.co.uk>
2019-06-12 09:50:21 +0100

    haskell/twelve-days - 1st draft

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -1,6 +1,6 @@
 module TwelveDays (recite) where
 
-import Data.List (unwords, intersperse)
+import Data.List (intersperse)
 
 days :: [String]
 days = [
@@ -12,25 +12,27 @@ days = [
     ,"fifth"
     ,"sixth"
     ,"seventh"
-    ,"eigth"
+    ,"eighth"
     ,"ninth"
     ,"tenth"
     ,"eleventh"
     ,"twelfth"
     ]
 
-onThe = "On the " :: String
+onThe :: String
+onThe = "On the "
 
+dayOf :: String
 dayOf = " day of Christmas my true love gave to me:"
 
 inventoryStr :: String
-inventoryStr = "twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, a Partridge in a Pear Tree"
+inventoryStr = " twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, a Partridge in a Pear Tree"
 
 recite :: Int -> Int -> [String]
 recite start stop = map reciteForDay [start .. stop]
 
 reciteForDay :: Int -> String
-reciteForDay d = onThe ++ (show (days !! d)) ++ dayOf ++ (getPresentsForDay d)
+reciteForDay d = onThe ++ (days !! d) ++ dayOf ++ (getPresentsForDay d) ++ "."
 
 inventory :: [String]
 inventory = toList inventoryStr
@@ -39,11 +41,6 @@ toList :: String -> [String]
 toList [] = []
 toList xs = chunk : (toList $ (drop $ length chunk + 1) xs)
     where chunk = takeWhile (/= ',') xs
-
-
-getText :: Int -> Int -> [String]
-getText start stop = [preamble]
-    where preamble = onThe ++ (days !! stop) ++ dayOf
 
 getPresentsForDay :: Int -> String
 getPresentsForDay d = concat $ intersperse "," $ reverse $ addAnd $ reverse $ drop (12 - d) inventory
