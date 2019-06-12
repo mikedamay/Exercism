34d385d70ec2f803f589e31f1e38ab09d733dc2e
mikedamay <mikemay@blueyonder.co.uk>
2019-06-12 18:20:47 +0100

    haskell/pangram - for publication

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,13 @@
 module Pangram (isPangram) where
 
+import Data.List (sort, nub)
+import Data.Char (isLetter, toLower)
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text = (prepare text) == alphabet
+
+alphabet :: String
+alphabet = ['a'..'z']
+
+prepare :: String -> String
+prepare = (nub . sort . map toLower . filter isLetter )
