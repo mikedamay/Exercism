a15d67b621f5d611fb25a6483a870c6c40ffb769
mikedamay <mikemay@blueyonder.co.uk>
2019-06-13 19:14:43 +0100

    csharp/largest-series-product - 1st draft

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Linq;
 
 public static class LargestSeriesProduct
 {
     public static long GetLargestProduct(string digits, int span) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int DigitToInt(char digit) =>  Char.IsDigit(digit) ? digit - '0' : throw new ArgumentException();
+        
+        if (span < 0 || span != 0 && span > digits.Length) throw new ArgumentException();
+
+        if (span == 0) return 1;
+
+        return Enumerable.Range(0, digits.Length - span + 1)
+            .SelectMany(n => digits.Substring(n, span), (n, c) => (n, DigitToInt(c)))
+            .GroupBy(p => p.Item1).Select( g => g
+            .Aggregate((acc, p) => (p.Item1, acc.Item2 * p.Item2)).Item2).Max();
+
     }
 }
