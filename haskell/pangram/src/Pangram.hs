8fe91f9730744a8cbcbda42061d412c046523e52
mikedamay <mikemay@blueyonder.co.uk>
2019-06-16 06:12:59 +0100

    haskell/pangram - tweaks

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -4,6 +4,6 @@ import Data.Set as Set (fromList, isSubsetOf)
 import Data.Char (toLower)
 
 isPangram :: String -> Bool
-isPangram text = Set.fromList alphabet `Set.isSubsetOf` (Set.fromList $ map toLower text)
+isPangram text = fromList alphabet `isSubsetOf` (fromList $ map toLower text)
     where alphabet = ['a'..'z']
 
