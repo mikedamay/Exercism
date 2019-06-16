cd425b649aaa99cfcefc497bd89a74fc58be98b8
mikedamay <mikemay@blueyonder.co.uk>
2019-06-16 06:48:03 +0100

    haskell/saddle-points - converted matrix element value to a type value

--- haskell/saddle-points/src/Matrix.hs
+++ haskell/saddle-points/src/Matrix.hs
@@ -19,45 +19,48 @@ tester3 :: [[Int]]
 tester3 = [ [3, 1, 3]
             , [3, 2, 4] ]
 
-data MatrixElement = MatrixElement ((Int, Int), Int) deriving (Eq)
+data MatrixElement e = MatrixElement ((Int, Int), e) deriving (Eq)
 
-saddlePoints :: Array (Int, Int) Int -> [(Int, Int)]
+-- -- saddlePoints :: Array (Int, Int) Int -> [(Int, Int)]
+-- -- saddlePoints matrix = error ""
+-- --
+saddlePoints :: (Ord e, Eq e) => Array (Int, Int) e -> [(Int, Int)]
 saddlePoints matrix = prepare $ listFromMatrix matrix
 
-prepare :: [[MatrixElement]] -> [(Int, Int)]
+prepare :: (Ord e, Eq e) => [[MatrixElement e]] -> [(Int, Int)]
 prepare xs = map (\(MatrixElement ((r, c), v)) -> (r, c)) $ concat $ getSaddlePoints [(x, y) | x <- getMaxElements xs, y <- getMinElements $ transpose xs ]
 
-getSaddlePoints :: [([MatrixElement],[MatrixElement])] -> [([MatrixElement])]
+getSaddlePoints :: (Ord e, Eq e) => [([MatrixElement e],[MatrixElement e])] -> [([MatrixElement e])]
 getSaddlePoints = map getSaddlePoint
     where
-        getSaddlePoint :: ([MatrixElement], [MatrixElement]) -> ([MatrixElement])
+        getSaddlePoint :: (Ord e, Eq e) => ([MatrixElement e], [MatrixElement e]) -> ([MatrixElement e])
         getSaddlePoint (rows, cols) = [ r | r <- rows, c <- cols, r == c]
 
-getMaxElements :: [[MatrixElement]] -> [[MatrixElement]]
+getMaxElements :: (Ord e) => [[MatrixElement e]] -> [[MatrixElement e]]
 getMaxElements = map (foldl findMax [])
     where findMax = saddlePoint (>)
 
-getMinElements :: [[MatrixElement]] -> [[MatrixElement]]
+getMinElements :: (Ord e) => [[MatrixElement e]] -> [[MatrixElement e]]
 getMinElements = map (foldl findMin [])
     where findMin = saddlePoint (<)
 
-saddlePoint :: (Int -> Int -> Bool) -> [MatrixElement] -> MatrixElement -> [MatrixElement]
+saddlePoint :: (Ord e) => (e -> e -> Bool) -> [MatrixElement e] -> (MatrixElement e) -> [MatrixElement e]
 saddlePoint fc [] new = [new]
 saddlePoint fc acc@((MatrixElement ((racc, cacc), vacc)):xs) new@(MatrixElement((r, c), v))
     | fc v vacc = [MatrixElement ((r, c), v)]
     | v == vacc = new:acc
     | otherwise = acc
 
-listFromMatrix :: (Array (Int, Int) Int) -> [[MatrixElement]]
+listFromMatrix :: (Ord e) => (Array (Int, Int) e) -> [[MatrixElement e]]
 listFromMatrix m = myChunksOf (getNumCols m) $ map elementFromMatrix $ assocs m
     where
-        getNumCols :: (Array (Int, Int) Int) -> Int
+        getNumCols :: (Array (Int, Int) e) -> Int
         getNumCols  m = (+) 1 $ snd $ snd $ bounds m
         myChunksOf :: Int -> [a] -> [[a]]
         myChunksOf _ [] = []
         myChunksOf n xs = (take n xs) : (myChunksOf n (drop n xs))
 
-elementFromMatrix :: ((Int, Int), Int) -> MatrixElement
+elementFromMatrix :: (Ord e) => ((Int, Int), e) -> (MatrixElement e)
 elementFromMatrix em = MatrixElement em
 
 matrixFromList xss =
