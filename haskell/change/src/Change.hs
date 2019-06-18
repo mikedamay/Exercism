bab461d6431c3e79007414c362fd0eb0ffe859c3
mikedamay <mikemay@blueyonder.co.uk>
2019-06-18 13:10:42 +0100

    haskell/change - passes all tests except too slow for 999 test

--- haskell/change/src/Change.hs
+++ haskell/change/src/Change.hs
@@ -1,4 +1,32 @@
 module Change (findFewestCoins) where
 
+import Data.List (sort, foldl1)
+
 findFewestCoins :: Integer -> [Integer] -> Maybe [Integer]
-findFewestCoins target coins = error "You need to implement this function."
+findFewestCoins 0 _ = Just []
+findFewestCoins target coins = if null used then Nothing else Just used
+    where used = doMax target $ doStuff target (reverse $ sort coins)
+
+doStuff :: Integer -> [Integer] -> [[Integer]]
+doStuff _ [] = []
+doStuff target xs = map (makeChange target) (setsOf xs)
+
+makeChange :: Integer -> [Integer] -> [Integer]
+makeChange _ [] = []
+makeChange target (x:xs)
+    | target == x = [x]
+    | target < x = doMax target $ doStuff target xs
+    | otherwise = x:(doMax (target - x) $ doStuff (target - x) (x:xs))
+
+getMax :: Integer -> [[Integer]] -> [Integer]
+getMax _ [] = []
+getMax target xss = foldl1 (\acc x -> if length x < length acc then x else acc ) $ xss
+
+setsOf :: [Integer] -> [[Integer]]
+setsOf [] = []
+setsOf (x:xs) = (x:xs):(setsOf xs)
+
+doMax :: Integer -> [[Integer]] -> [Integer]
+doMax target xss =
+    getMax target filtered
+    where filtered = filter (\xs -> (sum xs) == target) xss
