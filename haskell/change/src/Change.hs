890b5ed60d682b65af13c271fc485b6425581b12
mikedamay <mikemay@blueyonder.co.uk>
2019-06-19 11:36:28 +0100

    haskell/change - minor refactoring - still pretty poor

--- haskell/change/src/Change.hs
+++ haskell/change/src/Change.hs
@@ -4,7 +4,9 @@ import Data.List (sort, foldl1)
 
 findFewestCoins :: Integer -> [Integer] -> Maybe [Integer]
 findFewestCoins 0 _ = Just []
-findFewestCoins target denoms = if null used then Nothing else Just used
+findFewestCoins target denoms = case used of
+                                     [] -> Nothing
+                                     _ -> Just used
     where used = findFewest target $ forEachDenom target (reverse $ sort denoms)
 
 forEachDenom :: Integer -> [Integer] -> [[Integer]]
@@ -19,14 +21,12 @@ makeChange target (denom:denoms)
     | target > denom * 3 && denoms `areAllFactorsOf` denom = denom:(makeChange (target - denom) (denom:denoms))
     | otherwise = denom:(findFewest (target - denom) $ forEachDenom (target - denom) (denom:denoms))
 
-fewest :: [[Integer]] -> [Integer]
-fewest [] = []
-fewest xss = foldl1 (\acc x -> if length x < length acc then x else acc ) $ xss
-
 findFewest :: Integer -> [[Integer]] -> [Integer]
 findFewest target xss =
-    fewest filtered
-    where filtered = filter (\xs -> (sum xs) == target) xss
+    fewest $ filter targetMet xss
+    where targetMet = (== target) . sum
+          fewest [] = []
+          fewest xss' = foldl1 (\acc x -> if length x < length acc then x else acc ) xss'
 
 setsOf :: [Integer] -> [[Integer]]
 setsOf [] = []
