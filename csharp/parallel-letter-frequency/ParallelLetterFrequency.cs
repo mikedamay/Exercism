a5dcd0e325030b90078e324312171b3151063e41
mikedamay <mikemay@blueyonder.co.uk>
2019-06-21 08:38:45 +0100

    csharp/parallel-letter-frequency - early version

---
+++ csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Xml.Serialization;
+
+public static class ParallelLetterFrequency
+{
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
+    {
+        var xx = texts
+            .Select(t => t.ToLower())
+            .SelectMany(t => t, (t, c) => c)
+            .Where(Char.IsLetter)
+            .OrderBy(_ => _);
+        var xxx = xx.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+        return xxx;
+    }
+}
