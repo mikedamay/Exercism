0559565ee96e7237472668d021533bc620c594c8
mikedamay <mikemay@blueyonder.co.uk>
2019-06-23 14:28:02 +0100

    haskell/go-counting - builds WIP backup

--- haskell/go-counting/src/Counting.hs
+++ haskell/go-counting/src/Counting.hs
@@ -1,16 +1,59 @@
 module Counting (
     Color(..),
     territories,
-    territoryFor
+    territoryFor,
 ) where
 
-import Data.Set (Set)
+import qualified Data.Set as Set
+import qualified Data.Array as Array
 
-data Color = Black | White deriving (Eq, Ord, Show)
+data Color = Black | White | None deriving (Eq, Ord, Show)
 type Coord = (Int, Int)
 
-territories :: [String] -> [(Set Coord, Maybe Color)]
+territories :: [String] -> [(Set.Set Coord, Maybe Color)]
 territories board = error "You need to implement this function."
 
-territoryFor :: [String] -> Coord -> Maybe (Set Coord, Maybe Color)
+territoryFor :: [String] -> Coord -> Maybe (Set.Set Coord, Maybe Color)
 territoryFor board coord = error "You need to implement this function."
+
+
+findTerritories :: Array.Array (Int, Int) Color -> [Set.Set (Int, Int, Color)]
+findTerritories arr = [Set.empty]
+
+-- polishTerritories :: [Set.Set (Int, Int, Color)] -> [(Set (Int, Int), Color)]
+-- polishTerritories [] = []
+-- polishTerritories sets
+--     | length $ nub $ filter (\(x, y, c) -> c /= None) > 1 sets =
+--
+-- polishTerritory :: Set.Set (Int, Int, Color) -> (Set (Int, Int), Color)
+-- polishTerritory [] = []
+-- polishTerritory set
+--     | numColors > 1 = ((removeColor set), color)
+--   where
+--     numColors = length $ nub $ filter (\(x, y, c) -> c /= None) $ Set.toList set
+--     numColors = length $ nub $ filter (\(x, y, c) -> c /= None) $ Set.toList set
+
+territoryColor :: Set.Set (Int, Int, Color) -> Maybe Color
+territoryColor set | set == Set.empty = Nothing
+territoryColor set = foldl (\acc (x, y, c) -> if acc == Nothing then Nothing else
+                                                if acc == Just None then Just c else
+                                                    if c == None then acc else
+                                                        if Just c == acc then acc else
+                                                            Nothing
+                                                        ) (Just None)  (Set.toList set)
+
+stuff :: Set.Set (Int, Int, Color) -> [(Int, Int, Color)]
+stuff set = Set.toList set
+
+strToArray :: [String] -> Array.Array (Int, Int) Color
+strToArray lines = Array.listArray ((0, 0), (length lines - 1, lengthOfLine lines - 1))
+    $ map charToColor $ concat lines
+  where
+    charToColor 'B' = Black
+    charToColor 'W' = White
+    charToColor ' ' = None
+    charToColor _ = error "Unknown color"
+    lengthOfLine [] = 0
+    lengthOfLine ([]:xs) = 0
+    lengthOfLine (x:xs) = length x
+
