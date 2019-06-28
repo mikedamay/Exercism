c1c991f12a2663bea7afef492d17ac2a0479c47c
mikedamay <mikemay@blueyonder.co.uk>
2019-06-28 10:43:12 +0100

    haskell/bob - draft

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,16 @@
 module Bob (responseFor) where
 
+import Data.List.Extra (trim)
+import Data.Char (isUpper, isLower, isLetter)
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+    | null trimmed = "Fine. Be that way!"
+    | last trimmed == '?' && isYelling = "Calm down, I know what I'm doing!"
+    | last trimmed == '?' = "Sure."
+    | isYelling = "Whoa, chill out!"
+    | otherwise = "Whatever."
+  where
+    trimmed = trim xs
+    isYelling2 = any isLetter trimmed && (all isUpper $ filter isLetter trimmed)
+    isYelling = not (any isLower trimmed) && any isLetter trimmed
