dd0b225c491d7cca8fd70f6e8383156eda2f6503
mikedamay <mikemay@blueyonder.co.uk>
2019-06-28 12:23:16 +0100

    haskell/run-length-encoding - encode coded

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,18 @@
 module RunLength (decode, encode) where
 
+
+
 decode :: String -> String
 decode encodedText = error "You need to implement this function."
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode text = (show count) ++ encoded
+  where
+    (_, count, encoded) = foldr rlencode ('0', 0, []) text
+
+rlencode :: Char -> (Char, Int, [Char]) -> (Char, Int, [Char])
+rlencode c ('0', 0, []) = (c, 1, [c])
+rlencode c (ca, n, l)
+    | ca == c = (c, n+1, c:l)
+    | otherwise = (c, 1, (show n) ++ l)
+
