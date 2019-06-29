058a754ae32400d464a29f2b256014dc9f5458f8
mikedamay <mikemay@blueyonder.co.uk>
2019-06-29 04:33:11 +0100

    haskell/run-length-encoding - tests pass

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,18 +1,27 @@
 module RunLength (decode, encode) where
 
+import Data.Char (isDigit)
 
 
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode encodedText = concat $ map buildString $ foldr rldecode [] encodedText
 
 encode :: String -> String
-encode text = (show count) ++ encoded
+encode text = if count > 1 then (show count) ++ encoded else encoded
   where
     (_, count, encoded) = foldr rlencode ('0', 0, []) text
 
 rlencode :: Char -> (Char, Int, [Char]) -> (Char, Int, [Char])
 rlencode c ('0', 0, []) = (c, 1, [c])
 rlencode c (ca, n, l)
-    | ca == c = (c, n+1, c:l)
-    | otherwise = (c, 1, (show n) ++ l)
+    | ca == c = (c, n+1, l)
+    | otherwise = (c, 1, c:(if n > 1 then (show n) ++ l else l))
+
+rldecode :: Char -> [([Char], Char)] -> [([Char], Char)]
+rldecode c [] = [("", c)]
+rldecode c ((ct, ch):xs) | isDigit c = (c:ct, ch):xs
+rldecode c xs = ("", c):xs
+
+buildString :: ([Char], Char) -> [Char]
+buildString (num, ch) = ch : if length num > 0 then (replicate ((read num) - 1) ch) else ""
 
