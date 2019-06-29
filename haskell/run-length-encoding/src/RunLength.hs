04614d9d92629650bf33d9686d5237510d5b3a45
mikedamay <mikemay@blueyonder.co.uk>
2019-06-29 06:33:15 +0100

    haskell/run-length-encoding - better encode

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -7,15 +7,27 @@ decode :: String -> String
 decode encodedText = concat $ map buildString $ foldr rldecode [] encodedText
 
 encode :: String -> String
-encode text = if count > 1 then (show count) ++ encoded else encoded
+encode text = concat $ map encodingToText $ foldr rle [] text
   where
-    (_, count, encoded) = foldr rlencode ('0', 0, []) text
+    rle c [] = [(c, 1)]
+    rle c (all@(ac, n):xs)
+        | ac == c = (ac, n+1) : xs
+        | otherwise = (c, 1) : all : xs
+    encodingToText (c, 1) = [c]
+    encodingToText (c, n) = (show n) ++ (replicate n c)
 
 rlencode :: Char -> (Char, Int, [Char]) -> (Char, Int, [Char])
 rlencode c ('0', 0, []) = (c, 1, [c])
 rlencode c (ca, n, l)
     | ca == c = (c, n+1, l)
     | otherwise = (c, 1, c:(if n > 1 then (show n) ++ l else l))
+
+rlencode2 :: [(Char, Int)] -> String -> [(Char, Int)]
+rlencode2 xs [] = xs
+rlencode2 [] (c:cs) = rlencode2 [(c, 1)] cs
+rlencode2 (all@(ac, n):xs) (c:cs)
+    | ac == c = rlencode2 ((ac, n+1):xs) cs
+    | otherwise = rlencode2 ((c, 1):all:xs) cs
 
 rldecode :: Char -> [([Char], Char)] -> [([Char], Char)]
 rldecode c [] = [("", c)]
