5c897e866fe204ec2b21c28727d6ff58053ea1a9
mikedamay <mikemay@blueyonder.co.uk>
2019-07-04 14:09:21 +0100

    haskell/house - 1st draft

--- haskell/house/src/House.hs
+++ haskell/house/src/House.hs
@@ -1,7 +1,9 @@
 module House (rhyme) where
 
-rhyme :: String
-rhyme = "This is the house that Jack built.\n\
+import Data.List (intersperse)
+
+rhymex :: String
+rhymex = "This is the house that Jack built.\n\
         \\n\
         \This is the malt\n\
         \that lay in the house that Jack built.\n\
@@ -90,3 +92,47 @@ rhyme = "This is the house that Jack built.\n\
         \that killed the rat\n\
         \that ate the malt\n\
         \that lay in the house that Jack built.\n"
+
+this_is :: String
+this_is = "This is"
+that :: String
+that = "that"
+num_players :: Int
+num_players = length players
+max_player :: Int
+max_player = length players - 1
+
+
+players :: [(String, String)]
+{-
+players = [
+        ("ate", "the malt")
+        ,("lay in", "the house that Jack built.")
+        ]
+-}
+players = [
+        ("", "the horse and the hound and the horn")
+        ,("belonged to", "the farmer sowing his corn")
+        ,("kept", "the rooster that crowed in the morn")
+        ,("woke", "the priest all shaven and shorn")
+        ,("married", "the man all tattered and torn")
+        ,("kissed", "the maiden all forlorn")
+        ,("milked", "the cow with the crumpled horn")
+        ,("tossed", "the dog")
+        ,("worried", "the cat")
+        ,("killed", "the rat")
+        ,("ate", "the malt")
+        ,("lay in", "the house that Jack built.")
+        ]
+
+
+
+rhyme :: [Char]
+rhyme = concat $ concat $ intersperse ["\n"] $ map (\x -> [mkLine idx y | (idx, y) <- zip [0..] (drop (max_player - x) players)]) [0..max_player]
+
+mkLine :: Int -> (String, String) -> String
+mkLine 0 (_, player) = this_is ++ " " ++ player ++ "\n"
+mkLine _ (action, player) = glue that action player
+
+glue :: String -> String -> String -> String
+glue prefix action player = (concat $ intersperse " " [prefix, action, player]) ++ "\n"
