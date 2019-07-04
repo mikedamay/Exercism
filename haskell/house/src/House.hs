679992f6f0702259ae04d60264bb6100633774db
mikedamay <mikemay@blueyonder.co.uk>
2019-07-04 14:12:53 +0100

    haskell/house - for publication

--- haskell/house/src/House.hs
+++ haskell/house/src/House.hs
@@ -2,114 +2,15 @@ module House (rhyme) where
 
 import Data.List (intersperse)
 
-rhymex :: String
-rhymex = "This is the house that Jack built.\n\
-        \\n\
-        \This is the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the farmer sowing his corn\n\
-        \that kept the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the horse and the hound and the horn\n\
-        \that belonged to the farmer sowing his corn\n\
-        \that kept the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n"
-
 this_is :: String
 this_is = "This is"
 that :: String
 that = "that"
-num_players :: Int
-num_players = length players
 max_player :: Int
 max_player = length players - 1
 
 
 players :: [(String, String)]
-{-
-players = [
-        ("ate", "the malt")
-        ,("lay in", "the house that Jack built.")
-        ]
--}
 players = [
         ("", "the horse and the hound and the horn")
         ,("belonged to", "the farmer sowing his corn")
@@ -128,11 +29,8 @@ players = [
 
 
 rhyme :: [Char]
-rhyme = concat $ concat $ intersperse ["\n"] $ map (\x -> [mkLine idx y | (idx, y) <- zip [0..] (drop (max_player - x) players)]) [0..max_player]
+rhyme = concat $ concat $ intersperse ["\n"] $ map (\n -> [mkLine idx player | (idx, player) <- zip [0..] (drop (max_player - n) players)]) [0..max_player]
 
 mkLine :: Int -> (String, String) -> String
 mkLine 0 (_, player) = this_is ++ " " ++ player ++ "\n"
-mkLine _ (action, player) = glue that action player
-
-glue :: String -> String -> String -> String
-glue prefix action player = (concat $ intersperse " " [prefix, action, player]) ++ "\n"
+mkLine _ (action, player) = (concat $ intersperse " " [that, action, player]) ++ "\n"
