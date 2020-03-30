57d34e49bc81d8794fa75cd68fdf6a55bb58a71b
mikedamay <github-actions[bot]@users.noreply.github.com>
2020-03-30 15:05:16 +0100

    chsharp/robot-simulator - riffing on David Godfrey's solution

--- csharp/robot-simulator/RobotSimulator.cs
+++ csharp/robot-simulator/RobotSimulator.cs
@@ -1,45 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 public enum Direction
 {
-    North,
-    East,
-    South,
-    West
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3
 }
+
+
 
 public class RobotSimulator
 {
+    private struct State
+    {
+        public Direction Direction { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public State(Direction direction, int x, int y)
+        {
+            Direction = direction;
+            X = x;
+            Y = y;
+        }
+    }
+
+    public Direction Direction => state.Direction;
+    public int X => state.X;
+    public int Y => state.Y;
+    
+    private State state;
     public RobotSimulator(Direction direction, int x, int y)
     {
+        state = new State(direction, x, y);
     }
 
-    public Direction Direction
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
 
-    public int X
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
 
-    public int Y
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+    public void Move(string instructions) => instructions
+        .Select<char, Action>(instruction => instruction switch
+            {
+            'L' => TurnLeft,
+            'R' => TurnRight,
+            'A' => Advance,
+            _ => throw new ArgumentException()
+            })
+        .ToList()
+        .ForEach(operation => operation.Invoke());
 
-    public void Move(string instructions)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+
+    private void Advance() =>
+        (state.X, state.Y) = state.Direction switch
+            {
+            Direction.North => (state.X, state.Y + 1),
+            Direction.East => (state.X + 1, state.Y),
+            Direction.South => (state.X, state.Y - 1),
+            Direction.West => (state.X - 1, state.Y),
+            _ => throw new ArgumentOutOfRangeException()
+            };
+
+
+    private void TurnLeft() => state.Direction = (Direction) (((int) state.Direction + 3) % 4);
+    private void TurnRight() => state.Direction = (Direction) (((int) state.Direction + 1) % 4);
 }
