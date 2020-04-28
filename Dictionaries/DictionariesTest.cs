e44acddbdfdfc182350766a17c6ee187116e7eed
mikedamay <github-actions[bot]@users.noreply.github.com>
2020-04-28 10:08:42 +0100

    csharp/dictionaries - initial upload

---
+++ Dictionaries/DictionariesTest.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Dictionaries_Code;
+using Xunit;
+
+public class DictionariesTest
+{
+    [Fact]
+    public void TestEmptyDictionary()
+    {
+        Assert.Empty(Example.GetEmptyDiectionary());
+    }
+    [Fact(Skip = "")]
+    public void TestExistingDictionary()
+    {
+        var ed = Example.GetExistingDictionary();
+        Assert.Equal(3, ed.Count);
+        Assert.Equal("United States of America", ed[1]);
+        Assert.Equal("Brazil", ed[55]);
+        Assert.Equal("India", ed[91]);
+    }
+    [Fact(Skip = "")]
+    public void TestAddCountryToEmptyDictionary()
+    {
+        var ed = Example.AddCountryToEmptyDictionary(44, "United Kingdom");
+        Assert.Equal(4, ed.Count);
+        Assert.Equal("United States of America", ed[1]);
+        Assert.Equal("United Kingdom", ed[44]);
+        Assert.Equal("Brazil", ed[55]);
+        Assert.Equal("India", ed[91]);
+    }
+    [Fact(Skip = "")]
+    public void TestAddCountryToExistingDictionary()
+    {
+        var ed = Example.AddCountryToExistingDictionary(
+            Example.GetExistingDictionary(), 44, "United Kingdom");
+        Assert.Equal(4, ed.Count);
+        Assert.Equal("United States of America", ed[1]);
+        Assert.Equal("United Kingdom", ed[44]);
+        Assert.Equal("Brazil", ed[55]);
+        Assert.Equal("India", ed[91]);
+    }
+}
---
+++ Dictionaries/Example.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+
+namespace Dictionaries_Code
+{
+    public static class Example
+    {
+        public static Dictionary<int, string> GetEmptyDiectionary()
+        {
+            return new Dictionary<int, string>();   
+        }
+
+        public static Dictionary<int, string> GetExistingDictionary()
+        {
+            return new Dictionary<int, string>
+            {
+                {1, "United States of America"}, 
+                {55, "Brazil"}, 
+                {91, "India"}
+            };
+        }
+
+        public static Dictionary<int, string> AddCountryToEmptyDictionary(int CountryCode, string CountryName)
+        {
+            return new Dictionary<int, string>() { {CountryCode, CountryName}};
+        }
+
+        public static Dictionary<int, string> AddCountryToExistingDictionary(
+            Dictionary<int, string> existingDictiopnary, int countryCode, string CountryName)
+        {
+            existingDictiopnary[countryCode] = CountryName;
+            return existingDictiopnary;
+        }
+    }
+}
