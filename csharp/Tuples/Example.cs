02b996f3654aec9c54ff28798f60a2cbd7234259
mikedamay <mikemay.mdamay@googlemail.com>
2020-06-08 08:31:31 +0100

    csharp/tupes illustrations for issues

---
+++ csharp/Tuples/Example.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Tuples
+{
+public static (bool IsSquare, bool IsCube, int Product) DetectPowers(int num1, int num2, int num3){...}
+
+public static int ExtractProduct((bool, bool, bool) powersDetected) {...}
+    
+}
