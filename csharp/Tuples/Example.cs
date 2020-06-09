89c92d0cd61fc5d79433865f35f765cd4fcce134
mikedamay <mikemay.mdamay@googlemail.com>
2020-06-09 10:18:04 +0100

    csharp/tuples coding a bit

--- csharp/Tuples/Example.cs
+++ csharp/Tuples/Example.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
-public class Tuples
+public class Det
 {
-public static (bool IsSquare, bool IsCube, int Product) DetectPowers(int num1, int num2, int num3){...}
+    struct mike
+    {
+        private int abc;
+        mike(int a)
+        {
+            abc = a;
+        }
+    }
+    public static (bool IsSquare, bool IsCube, int Product) DetectPowers(int num1, int num2, int num3)
+    {
+        var mike = (1, 2);
+        var bob = (1, 2);
+        var xxx = bob == mike;
+        return (true, true, 1);
+    }
 
-public static int ExtractProduct((bool, bool, bool) powersDetected) {...}
+    public static int ExtractProduct((bool, bool, bool) powersDetected)
+    {
+        return 1;
+    }
     
 }
---
+++ csharp/Tuples/TuplesTests.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+public class TuplesTest
+{
+    [Fact(Skip = "Remove this Skip property to run this test")]
+    public void Clean_single_letter()
+    {
+        
+    }
+
+}
