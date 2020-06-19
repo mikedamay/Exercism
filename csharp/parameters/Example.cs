ec60f10c70f5493c7f16dd9521efe9e33f2c8639
mikedamay <mikemay.mdamay@googlemail.com>
2020-06-19 04:58:33 +0100

    csharp/parameters initial commit

---
+++ csharp/parameters/Example.cs
@@ -0,0 +1,7 @@
+namespace Parameters
+{
+    public class Example
+    {
+        
+    }
+}
---
+++ csharp/parameters/ParametersTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+public class EqualityTests
+{
+
+//    [Fact]
+    public void IsAdmin_with_admin()
+    {
+    }
+
+    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    public void IsAdmin_with_wrong_email()
+    {
+    }
+
+}    
