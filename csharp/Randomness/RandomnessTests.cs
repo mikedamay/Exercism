26727c2b71428f2beb0a1df606ac89382683c5c1
mikedamay <mikemay.mdamay@googlemail.com>
2020-06-19 12:17:31 +0100

    csharp/randomness code complete

--- csharp/Randomness/RandomnessTests.cs
+++ csharp/Randomness/RandomnessTests.cs
@@ -1,7 +1,21 @@
-namespace Randomness
+using Xunit;
+using System;
+
+public class UserDefinedExceptionsTests
 {
-    public class RandomnessTests
+    [Fact]
+    public void RollDie()
     {
-        
+        var player = new Player();
+        var pips = player.RollDie();
+        Assert.InRange(pips, 1, 18);
+    }
+
+    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    public void GenerateSpellStrength()
+    {
+        var player = new Player();
+        var strength = player.GenerateSpellStrength();
+        Assert.InRange(strength, 0.0, 100.0);
     }
 }
