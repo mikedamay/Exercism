d8531542fda617e8cc4cb60212843dce84bfc0df
mikedamay <mikemay.mdamay@googlemail.com>
2020-06-24 09:50:05 +0100

    csharp/randomness cosmetics

--- csharp/Randomness/Randomness.cs
+++ csharp/Randomness/Randomness.cs
@@ -8,6 +8,7 @@ namespace template
         {
             throw new NotImplementedException("Please implement the Player.RollDie() method");
         }
+
         public double GenerateSpellStrength()
         {
             throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
