dd6e4191be335b7fe92d28b1b08bd902b6c85411
mikedamay <mikemay.mdamay@googlemail.com>
2020-06-27 12:05:57 +0100

    csharp/parameters tests complete subject to good story

--- csharp/object-initialization/Example.cs
+++ csharp/object-initialization/Example.cs
@@ -1,9 +1,12 @@
+namespace example
+{
 using System;
 using System.Collections.Generic;
 
+//**** please do not modify the FacialFeatures class ****
 public class FacialFeatures
 {
-    public string EyeColor { get; set;  }
+    public string EyeColor { get; set; }
     public decimal PhiltrumWidth { get; set; }
 
     public FacialFeatures()
@@ -11,12 +14,13 @@ public class FacialFeatures
     }
 }
 
+//**** please do not modify the Identity class ****
 public class Identity
 {
     public string Email { get; set; }
     public FacialFeatures FacialFeatures { get; set; }
     public IList<string> NameAndAddress { get; set; }
-    
+
     public Identity()
     {
     }
@@ -24,47 +28,46 @@ public class Identity
 
 public class Authenticator
 {
-    private Identity admin = new Identity{
+    public Identity Admin { get; } = new Identity
+    {
         Email = "admin@exerc.ism",
         FacialFeatures = new FacialFeatures
         {
             EyeColor = "green",
             PhiltrumWidth = 0.9m
         },
-        NameAndAddress = {"Chanakya Niti Kautilya Arthashastra", "Plausible Address", "Mombai"}
+        NameAndAddress = new List<string>{"Chanakya Niti Kautilya Arthashastra", "Plausible Address", "Mombai"}
     };
 
-    private IDictionary<string, Identity> developers
+    public IDictionary<string, Identity> Developers { get; }
         = new Dictionary<string, Identity>
         {
-            ["bert"] = new Identity{
+            ["bert"] = new Identity
+            {
                 Email = "bert@exerc.ism",
                 FacialFeatures = new FacialFeatures
                 {
                     EyeColor = "green",
                     PhiltrumWidth = 0.9m
                 },
-                NameAndAddress = {"Bertrand Meyer", "Avenue des Champs-Élysées", "Paris"}
-                },
-            
-            ["anders"] = new Identity{
+                NameAndAddress = new List<string>{"Bertrand Meyer", "Avenue des Champs-Élysées", "Paris"}
+            },
+
+            ["anders"] = new Identity
+            {
                 Email = "anders@exerc.ism",
                 FacialFeatures = new FacialFeatures
                 {
                     EyeColor = "green",
                     PhiltrumWidth = 0.9m
                 },
-                NameAndAddress = {"Anders Hejlsberg", "Plausible Address", "Redmond"}
+                NameAndAddress = new List<string>{"Anders Hejlsberg", "Plausible Address", "Redmond"}
             }
         };
 
-    public Identity GetAdmin()
+    public void Foo()
     {
-        return admin;
+        var aa = new Identity{NameAndAddress = {"mike", "jon"}};
     }
-
-    public IDictionary<string, Identity> GetDevelopers()
-    {
-        return developers;
-    }
+}
 }
--- csharp/object-initialization/ObjectInitialization.cs
+++ csharp/object-initialization/ObjectInitialization.cs
@@ -1,7 +1,42 @@
-namespace ObjectInitialization
+namespace template
 {
-    public class ObjectInitialization
+    
+using System;
+using System.Collections.Generic;
+
+//**** please do not modify the FacialFeatures class ****
+public class FacialFeatures
+{
+    public string EyeColor { get; set;  }
+    public decimal PhiltrumWidth { get; set; }
+
+    public FacialFeatures()
     {
-        
     }
+}
+
+//**** please do not modify the Identity class ****
+public class Identity
+{
+    public string Email { get; set; }
+    public FacialFeatures FacialFeatures { get; set; }
+    public IList<string> NameAndAddress { get; set; }
+    
+    public Identity()
+    {
+    }
+}
+
+public class Authenticator
+{
+    public Identity GetAdmin()
+    {
+        throw new NotImplementedException("Please implement the Authenticator.GetAdmin() method");
+    }
+
+    public IDictionary<string, Identity> GetDevelopers()
+    {
+        throw new NotImplementedException("Please implement the Authenticator.GetDevelopers() method");
+    }
+}
 }
