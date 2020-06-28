9338a8bc2ff4c3d62819926ee3f26d4621f73921
mikedamay <mikemay.mdamay@googlemail.com>
2020-06-28 07:43:26 +0100

    csharp/switch-statements template file

--- csharp/switch-statements/SwitchStatements.cs
+++ csharp/switch-statements/SwitchStatements.cs
@@ -28,68 +28,12 @@ public static class PlayAnalyzer
 {
     public static string AnalyzeOnField(int shirtNum)
     {
-        string playerDescription = string.Empty;
-        switch (shirtNum)
-        {
-            case 1:
-                playerDescription = "goalie";
-                break;
-            case 2:
-                playerDescription = "left back";
-                break;
-            case 5:
-                playerDescription = "right back";
-                break;
-            case 3:
-            case 4:
-                playerDescription = "center back";
-                break;
-            case 6:
-            case 7:
-            case 8:
-                playerDescription = "midfielder";
-                break;
-            case 9:
-                playerDescription = "left wing";
-                break;
-            case 11:
-                playerDescription = "right wing";
-                break;
-            case 10:
-                playerDescription = "striker";
-                break;
-            default:
-                throw new ArgumentException();
-        }
-
-        return playerDescription;
+        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOnField() method");
     }
 
     public static string AnalyzeOffField(object report)
     {
-        string description = string.Empty;
-        switch (report)
-        {
-            case int shirtNum:
-                description = AnalyzeOnField(shirtNum);
-                break;
-            case string freeFormText:
-                description = freeFormText;
-                break;
-            case Incident incident:
-                description = incident.ToString();
-                break;
-            case Manager manager when !string.IsNullOrWhiteSpace(manager.Name):
-                description = manager.Name;
-                break;
-            case Manager manager:
-                description = "the manager";
-                break;
-            default:
-                throw new ArgumentException();
-        }
-
-        return description;
+        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOffField() method");
     }
 }
 }
