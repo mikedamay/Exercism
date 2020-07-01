c8d4ca30019fd99af0ad4e53603f41f9f34a6a8d
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-01 07:57:17 -0400

    csharp/times ready for final review

--- csharp/object-initialization/Example.cs
+++ csharp/object-initialization/Example.cs
@@ -3,29 +3,6 @@ namespace example
 using System;
 using System.Collections.Generic;
 
-//**** please do not modify the FacialFeatures class ****
-public class FacialFeatures
-{
-    public string EyeColor { get; set; }
-    public decimal PhiltrumWidth { get; set; }
-
-    public FacialFeatures()
-    {
-    }
-}
-
-//**** please do not modify the Identity class ****
-public class Identity
-{
-    public string Email { get; set; }
-    public FacialFeatures FacialFeatures { get; set; }
-    public IList<string> NameAndAddress { get; set; }
-
-    public Identity()
-    {
-    }
-}
-
 public class Authenticator
 {
     public Identity Admin { get; } = new Identity
@@ -64,10 +41,21 @@ public class Authenticator
                 NameAndAddress = new List<string>{"Anders", "Redmond", "USA"}
             }
         };
+}
 
-    public void Foo()
-    {
-        var aa = new Identity{NameAndAddress = {"mike", "jon"}};
-    }
+//**** please do not modify the FacialFeatures class ****
+public class FacialFeatures
+{
+    public string EyeColor { get; set; }
+    public decimal PhiltrumWidth { get; set; }
+}
+
+//**** please do not modify the Identity class ****
+public class Identity
+{
+    public string Email { get; set; }
+    public FacialFeatures FacialFeatures { get; set; }
+    public IList<string> NameAndAddress { get; set; }
 }
 }
+
