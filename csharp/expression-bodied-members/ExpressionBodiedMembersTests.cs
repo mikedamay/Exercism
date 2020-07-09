2a3f6427aa35c0dd8b1255714d3b22246fda64d3
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-09 12:33:59 -0400

    csharp/expression-bodied-members WIP

--- csharp/expression-bodied-members/ExpressionBodiedMembersTests.cs
+++ csharp/expression-bodied-members/ExpressionBodiedMembersTests.cs
@@ -1,4 +1,6 @@
+using System;
 using Xunit;
+using Xunit.Sdk;
 
 public class ExpressionBodiedMembersTests
 {
@@ -27,5 +29,63 @@ public class ExpressionBodiedMembersTests
         ws.AcceptReading(new Reading(20m, 25m, 0.01m));
         ws.AcceptReading(new Reading(21m, 25m, 0.00m));
         Assert.True(ws.HasHistory);
+    }
+
+    [Fact /*(Skip = "Remove this Skip property to run this test")*/]
+    public void ClearAll()
+    {
+        var ws = new WeatherStation();
+        ws.AcceptReading(new Reading(20m, 25m, 0.01m));
+        ws.AcceptReading(new Reading(21m, 25m, 0.00m));
+        ws.ClearAll();
+        object[] expected = {false, 0m};
+        object[] actual = {ws.HasHistory, ws.LatestTemperature};
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact /*(Skip = "Remove this Skip property to run this test")*/]
+    public void Outlook_exception()
+    {
+        var ws = new WeatherStation();
+        Assert.Throws<ArgumentException>(() => ws.Outlook);
+    }
+
+    [Fact /*(Skip = "Remove this Skip property to run this test")*/]
+    public void Outlook_cool()
+    {
+        var ws = new WeatherStation();
+        ws.AcceptReading(new Reading(7m, 7m, 0m));
+        Assert.Equal(Outlook.Cool, ws.Outlook);
+    }
+
+    [Fact /*(Skip = "Remove this Skip property to run this test")*/]
+    public void Outlook_good()
+    {
+        var ws = new WeatherStation();
+        ws.AcceptReading(new Reading(55m, 7m, 0m));
+        Assert.Equal(Outlook.Good, ws.Outlook);
+    }
+
+    [Fact /*(Skip = "Remove this Skip property to run this test")*/]
+    public void Outlook_warm()
+    {
+        var ws = new WeatherStation();
+        ws.AcceptReading(new Reading(40m, 7m, 0m));
+        Assert.Equal(Outlook.Warm, ws.Outlook);
+    }
+
+    [Fact /*(Skip = "Remove this Skip property to run this test")*/]
+    public void RunSelfTest_good()
+    {
+        var ws = new WeatherStation();
+        ws.AcceptReading(new Reading(40m, 7m, 0m));
+        Assert.Equal(State.Good, ws.RunSelfTest());
+    }
+
+    [Fact /*(Skip = "Remove this Skip property to run this test")*/]
+    public void RunSelfTest_bad()
+    {
+        var ws = new WeatherStation();
+        Assert.Equal(State.Bad, ws.RunSelfTest());
     }
 }
