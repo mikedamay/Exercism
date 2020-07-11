e976b3f720a8138dce8a55264057d87b1e38d22d
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-11 04:00:57 -0400

    csharp/nested-types code complete

--- csharp/nested-types/NestedTypesTests.cs
+++ csharp/nested-types/NestedTypesTests.cs
@@ -1,7 +1,23 @@
-namespace NestedTypes
+using Xunit;
+
+public class NestedTypeTests
 {
-    public class NestedTypesTests
+    [Fact]
+    public void ShowSponsor()
     {
-        
+        var car = new RemoteControlCar();
+        string expected = "Walker Industries Inc.";
+        car.Telemetry.ShowSponsor(expected);
+        Assert.Equal(expected, car.CurrentSponsor);
+    }
+
+    [Fact /*(Skip = "Remove this Skip property to run this test")*/]
+    public void ShowSpeed()
+    {
+        var car = new RemoteControlCar();
+        string expected = "100 meters per second";
+        decimal expectedAmount = 100;
+        car.Telemetry.SetSpeed(100, "mps");
+        Assert.Equal(expected, car.GetSpeed());
     }
 }
