86d96585ec2ce7a4c7e41ad7c0bccf314378f7d5
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-16 04:05:32 -0400

    csharp/lists prettier

--- csharp/lists/Example.cs
+++ csharp/lists/Example.cs
@@ -31,7 +31,7 @@ public static class Languages
     {
         return languages.Contains(language);
     }
-    
+
     public static List<string> ReverseList(List<string> languages)
     {
         languages.Reverse();
