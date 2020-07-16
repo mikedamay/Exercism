48d3d7bfd95433a1ba5816b14324268132ba96fb
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-16 03:56:05 -0400

    csharp/lists review points

--- csharp/lists/ListsTests.cs
+++ csharp/lists/ListsTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 public class ListsTests
@@ -8,52 +9,65 @@ public class ListsTests
         Assert.Empty(Languages.NewList());
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void ExistingList()
     {
-        Assert.Equal(new string[] {"C#", "Clojure", "Elm"}, Languages.GetExistingLanguages());
+        var expected = new List<string>();
+        expected.Add("C#");
+        expected.Add("Clojure");
+        expected.Add("Elm");
+        Assert.Equal(expected, Languages.GetExistingLanguages());
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void AddLanguage()
     {
-        Assert.Equal(new string[] {"C#", "Clojure", "Elm", "Bash"},
+        var expected = new List<string>();
+        expected.Add("C#");
+        expected.Add("Clojure");
+        expected.Add("Elm");
+        expected.Add("Bash");
+        Assert.Equal(expected,
             Languages.AddLanguage(Languages.GetExistingLanguages(), "Bash"));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void CountLanguages()
     {
         Assert.Equal(3,
             Languages.CountLanguages(Languages.GetExistingLanguages()));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void HasLanguage_yes()
     {
         Assert.True(Languages.HasLanguage(Languages.GetExistingLanguages(), "Elm"));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void HasLanguage_no()
     {
         Assert.False(Languages.HasLanguage(Languages.GetExistingLanguages(), "D"));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void ReverseList()
     {
-        Assert.Equal(new string[] {"Elm", "Clojure", "C#"},
+        var expected = new List<string>();
+        expected.Add("Elm");
+        expected.Add("Clojure");
+        expected.Add("C#");
+        Assert.Equal(expected,
             Languages.ReverseList(Languages.GetExistingLanguages()));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void ContainsStar_yes()
     {
         Assert.True(Languages.ContainsStar(Languages.GetExistingLanguages()));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void ContainsStar_too_many()
     {
         var languages = Languages.GetExistingLanguages();
@@ -61,14 +75,14 @@ public class ListsTests
         Assert.False(Languages.ContainsStar(languages));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void ContainsStar_empty()
     {
         var languages = Languages.NewList();
         Assert.False(Languages.ContainsStar(languages));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void ContainsStar_single_star()
     {
         var languages = Languages.GetExistingLanguages();
@@ -77,28 +91,35 @@ public class ListsTests
         Assert.True(Languages.ContainsStar(languages));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void RemoveLanguage_yes()
     {
+        var expected = new List<string>();
+        expected.Add("C#");
+        expected.Add("Elm");
         var languages = Languages.GetExistingLanguages();
-        Assert.Equal(new string[] {"C#", "Elm"}, Languages.RemoveLanguage(languages, "Clojure"));
+        Assert.Equal(expected, Languages.RemoveLanguage(languages, "Clojure"));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void RemoveLanguage_no()
     {
+        var expected = new List<string>();
+        expected.Add("C#");
+        expected.Add("Clojure");
+        expected.Add("Elm");
         var languages = Languages.GetExistingLanguages();
-        Assert.Equal(new string[] {"C#", "Clojure", "Elm"}, Languages.RemoveLanguage(languages, "English"));
+        Assert.Equal(expected, Languages.RemoveLanguage(languages, "English"));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void EnsureUnique_yes()
     {
         var languages = Languages.GetExistingLanguages();
         Assert.True(Languages.EnsureUnique(languages));
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void EnsureUnique_no()
     {
         var languages = Languages.GetExistingLanguages();
