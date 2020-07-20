be9c221809811bbf8d3af236b0cb59624b1ffa43
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-20 11:54:12 -0400

    import WIP

--- csharp/exercise-report/ExerciseFileCreator.cs
+++ csharp/exercise-report/ExerciseFileCreator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace ExerciseReport
 {
@@ -13,26 +14,69 @@ namespace ExerciseReport
 
         public ExerciseFile CreateExerciseFileFromConceptsDoc()
         {
-            (var importResult, var concepts, var errors) = importer.ImportOriginalConceptsDoc();
+            var conceptMap = new Dictionary<string, Concept>();
+            var exerciseMap = new Dictionary<string, Exercise>();
+            var exerciseFile = new ExerciseFile();
+            
+            (var importResult, var concepts, _) = importer.ImportOriginalConceptsDoc();
             if (importResult == ImportResult.Incomplete)
             {
-                throw new Exception("Too many errors were encountered importing the ocriginal concepts document");
+                throw new Exception("Too many errors were encountered importing the original concepts document");
             }
 
-            foreach (ImportedConcept concept in concepts)
+            foreach (ImportedConcept importedConcept in concepts)
             {
-                if (concept.CanonicalConceptName != string.Empty)
+                if (importedConcept.Rank <= 0 || importedConcept.Rank >= 100 && importedConcept.Rank != 800)
                 {
+                    continue;    // section headings, unclassified concepts, report headings
+                }
+                if (importedConcept.Rank >= 0 && importedConcept.Rank <= 100)
+                {
+                    OriginalConcept originalConcept = new OriginalConcept
+                    {
+                        Name = importedConcept.OriginalConceptName,
+                        LineNumber = importedConcept.ConceptsDocLineNum
+                    };
+                    Concept concept = new Concept
+                    {
+                        Name = importedConcept.CanonicalConceptName,
+                        OriginalConcepts = new List<OriginalConcept>{originalConcept}
+                    };
                     Exercise exercise = new Exercise
                     {
-                        Slug = concept.CanonicalConceptName,
-                        DocumentType = concept.DocType == "I" ? DocType.Issue :
-                            concept.DocType == "E" ? DocType.Design : DocType.None,
-                        DocumentLink = concept.Link
+                        Slug = importedConcept.CanonicalConceptName,
+                        DocumentType = importedConcept.DocType == "I" ? DocType.Issue :
+                            importedConcept.DocType == "E" ? DocType.Design : DocType.None,
+                        DocumentLink = importedConcept.Link,
+                        Concepts = new List<Concept>{concept}
                     };
+                    conceptMap[concept.Name] = concept;
+                    exerciseMap[exercise.Slug] = exercise;
+                }
+                else // importedConcept.Rank == 800
+                {
+                    OriginalConcept originalConcept = new OriginalConcept
+                    {
+                        Name = importedConcept.OriginalConceptName,
+                        LineNumber = importedConcept.ConceptsDocLineNum
+                    };
+                    Concept concept;
+                    if (conceptMap.ContainsKey(importedConcept.CanonicalConceptName))
+                    {
+                        concept = conceptMap[importedConcept.CanonicalConceptName];
+                    }
+                    else
+                    {
+                        concept = new Concept();
+                        Exercise exercise = exerciseMap[importedConcept.CanonicalConceptName];
+                        exercise.Concepts.Add(concept);
+                    }
+
+                    concept.Name = importedConcept.CanonicalConceptName;
+                    concept.OriginalConcepts.Add(originalConcept);
                 }
             }
-            return new ExerciseFile();
+            return exerciseFile;
         }
     }
 }
