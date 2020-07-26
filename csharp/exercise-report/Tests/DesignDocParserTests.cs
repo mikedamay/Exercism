ad4f8510f54e12b9cd092f2ef4cd7fdf4416f591
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-26 14:49:01 -0400

    csharp/exercise-report tests substantially complete

--- csharp/exercise-report/Tests/DesignDocParserTests.cs
+++ csharp/exercise-report/Tests/DesignDocParserTests.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using FSharp.Compiler;
 using Xunit;
 
 namespace ExerciseReport.Tests
@@ -11,8 +13,84 @@ namespace ExerciseReport.Tests
             var ddp = new DesignDocParser();
             var markdown = ExerciseReportTests.GetResourceAsString(
                 Constants.DesignBrokenConceptsResource);
-            var results = ddp.ParseDesignDoc("embedded-resource.md", markdown).ToList(); 
+            var results = ddp.ParseDesignDoc(Constants.DesignBrokenConceptsResource,
+                markdown).ToList(); 
             Assert.False(results[0].success);
+        }
+
+        [Fact]
+        public void ParseDesignDoc_WithConceptsWithoutBullets_ReportsErrors()
+        {
+            const string resourceName = Constants.DesignConceptsWithoutBulletsResource;
+            var ddp = new DesignDocParser();
+            var markdown = ExerciseReportTests.GetResourceAsString(resourceName);
+            var results = ddp.ParseDesignDoc(resourceName, markdown).ToList(); 
+            Assert.False(results[0].success);
+            Assert.Contains("no learning objectives", results[0].error);
+        }
+
+        [Fact]
+        public void ParseDesignDoc_WithConceptsWithoutQuotes_ReportsErrors()
+        {
+            const string resourceName = Constants.DesignConceptsWithoutQuotesResource;
+            var ddp = new DesignDocParser();
+            var markdown = ExerciseReportTests.GetResourceAsString(resourceName);
+            var results = ddp.ParseDesignDoc(resourceName, markdown).ToList(); 
+            Assert.False(results[0].success);
+            Assert.Contains("invalid format", results[0].error);
+        }
+
+        [Fact]
+        public void ParseDesignDoc_WithJustConcepts_ReportsErrors()
+        {
+            const string resourceName = Constants.DesignJustConceptsResource;
+            var ddp = new DesignDocParser();
+            var markdown = ExerciseReportTests.GetResourceAsString(resourceName);
+            var results = ddp.ParseDesignDoc(resourceName, markdown).ToList(); 
+            Assert.True(results[0].success);
+        }
+
+        [Fact]
+        public void ParseDesignDoc_WithMultipleHashes_ReportsErrors()
+        {
+            const string resourceName = Constants.DesignMultipleHashesResource;
+            var ddp = new DesignDocParser();
+            var markdown = ExerciseReportTests.GetResourceAsString(resourceName);
+            var results = ddp.ParseDesignDoc(resourceName, markdown).ToList(); 
+            Assert.True(results[0].success);
+        }
+
+        [Fact]
+        public void ParseDesignDoc_WithNoConceptSection_ReportsErrors()
+        {
+            const string resourceName = Constants.DesignNoConceptSectionResource;
+            var ddp = new DesignDocParser();
+            var markdown = ExerciseReportTests.GetResourceAsString(resourceName);
+            var results = ddp.ParseDesignDoc(resourceName, markdown).ToList(); 
+            Assert.False(results[0].success);
+            Assert.Contains("no learning objectives", results[0].error);
+        }
+
+        [Fact]
+        public void ParseDesignDoc_WithNoConcepts_ReportsErrors()
+        {
+            const string resourceName = Constants.DesignNoConceptsResource;
+            var ddp = new DesignDocParser();
+            var markdown = ExerciseReportTests.GetResourceAsString(resourceName);
+            var results = ddp.ParseDesignDoc(resourceName, markdown).ToList(); 
+            Assert.False(results[0].success);
+            Assert.Contains("no learning objectives", results[0].error);
+        }
+
+        [Fact]
+        public void ParseDesignDoc_WithNoConceptsTrailing_ReportsErrors()
+        {
+            const string resourceName = Constants.DesignNoConceptsTrailingResource;
+            var ddp = new DesignDocParser();
+            var markdown = ExerciseReportTests.GetResourceAsString(resourceName);
+            var results = ddp.ParseDesignDoc(resourceName, markdown).ToList(); 
+            Assert.False(results[0].success);
+            Assert.Contains("no learning objectives", results[0].error);
         }
     }
 }
