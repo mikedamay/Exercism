b9fce049c490cd657ea60b18d92b73fd58c17e86
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-27 02:36:39 -0400

    csharp/exercise-report WIP

--- csharp/exercise-report/Tests/ExerciseReportTests.cs
+++ csharp/exercise-report/Tests/ExerciseReportTests.cs
@@ -1,52 +1,11 @@
-using System;
-using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Xunit;
-using System.Text.Json;
 
 namespace ExerciseReport.Tests
 {
     public class ExerciseReportTests
     {
-        [Fact]
-        public void Parse_ExerciseFileMissingFields_ThrowsException()
-        {
-            var ejp = new ExerciseJsonParser();
-            ejp.FromString(Utils.GetResourceAsString(Constants.ExercisesMissingFieldsResource));
-        }
-        
-        [Fact]
-        public void Parse_WellFormedDesignDoc_ProducesConceptLearningObjectives()
-        {
-            const string SampleDesignDoc = Constants.SampleDesignResource;
-
-            var ddp = new DesignDocParser();
-            string markdownText = Utils.GetResourceAsString(SampleDesignDoc);
-
-            var lo = ddp.ParseDesignDoc("embedded-resource.md", markdownText).ToList();
-            Assert.NotEmpty(lo);
-        }
-
-        [Fact]
-        public void ListExercises_OnExercismGit_ProducesFullList()
-        {
-            const string ExercismRoot = Constants.TestUserRoot;
-            var ddc = new DesignDocCollator(new DesignDocFileHandler(ExercismRoot, "csharp"), new DesignDocParser());
-            var actual = ddc.GetLearningObjectives(Constants.CSharpTrack);
-        }
-
-        [Fact]
-        public void Merge_ExercisesAndDesignDocs_ShowsLearningObjectives()
-        {
-            string ExercismRoot = PathNames.Test.Root;
-            const string Track = Constants.CSharpTrack;
-            var em = new ExerciseMerger(Track, new ExerciseFileCollator( new ExerciseFileHandler(ExercismRoot, Track),
-                    new ExerciseJsonParser())
-                , new DesignDocCollator(new DesignDocResourceHandler(), new DesignDocParser()));
-            em.Merge();
-        }
-
         [Fact]
         public void Merge_DataWithFatalError_WritesNoExercises()
         {
@@ -100,16 +59,6 @@ namespace ExerciseReport.Tests
             var exerciseFile = merger.MergeLearningObjectives();
             var output = rr.CreateReport(exerciseFile.exerciseObjectTree);
             Assert.NotEmpty(output);
-        }
-
-        [Fact]
-        public void MakeDesignTestData() 
-        {
-            string Separator = Constants.DesignDocSeparator;
-            var ddfh = new DesignDocFileHandler(PathNames.Test.Root, "csharp");
-            var designs = ddfh.GetExerciseDesignsForTrack().ToList();
-            var designResource = string.Join(Separator, designs);
-            var output = Regex.Split(designResource, Separator);
         }
     }
 }
