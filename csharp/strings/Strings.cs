76e48deeb4c4bc2160d24584a0518206bf0230ff
mikedamay <mikemay.mdamay@googlemail.com>
2020-08-07 00:50:08 -0400

    csharp/strings initial commit

---
+++ csharp/strings/Strings.cs
@@ -0,0 +1,19 @@
+using System;
+
+static class LogLine
+{
+    public static string Message(string logLine)
+    {
+        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+    }
+
+    public static string LogLevel(string logLine)
+    {
+        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+    }
+
+    public static string Reformat(string logLine)
+    {
+        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+    }
+}
