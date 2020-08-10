1238dec94e741cce954c0635a69ca5681219c9cb
mikedamay <mikemay.mdamay@googlemail.com>
2020-08-10 03:54:27 -0400

    csharp/exercise-report 3rd draft complete

--- csharp/exercise-report/ExerciseMerger.cs
+++ csharp/exercise-report/ExerciseMerger.cs
@@ -62,7 +62,7 @@ namespace ExerciseReport
                 if (!exerciseMap.Contains(conceptDetails.ConceptName))
                 {
                     errors.Add(new Error(ErrorSource.Merge, Severity.Error,
-                        $"Failed to find concept {conceptDetails.ConceptName} from {conceptDetails.DocId} in exercises.json file"));
+                        $"Failed to find concept {conceptDetails.ConceptName}, from the {conceptDetails.DocId} design.md, in exercises.json file"));
                 }
             }
 
