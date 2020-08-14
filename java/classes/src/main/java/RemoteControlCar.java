5dca213f78cc3d6ae7e56ca7c14d8679383b6ed1
mikedamay <mikemay.mdamay@googlemail.com>
2020-08-14 04:12:23 -0400

    java/classes initial commit

---
+++ java/classes/src/main/java/RemoteControlCar.java
@@ -0,0 +1,19 @@
+/*
+public class RemoteControlCar {
+    public static RemoteControlCar Buy() {
+        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.Buy()  method");
+    }
+
+    public String DistanceDisplay() {
+        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.DistanceDisplay()  method");
+    }
+
+    public String BatteryDisplay() {
+        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.BatteryDisplay()  method");
+    }
+
+    public void Drive() {
+        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.Drive()  method");
+    }
+}
+*/
