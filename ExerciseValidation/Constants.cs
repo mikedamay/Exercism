05924dc86e3c06ce3a57339a526ee9611322ed5f
mikedamay <mikemay.mdamay@googlemail.com>
2020-09-13 01:25:55 -0400

    straighten project hierarchy

--- ExerciseValidation/Constants.cs
+++
@@ -1,8 +0,0 @@
-namespace ExerciseValidation
-{
-    public static class Constants
-    {
-        public const string TrackConfigFileName = "config.json";
-        public const string CSharpTrack = ExerciseReport.Constants.CSharpTrack;
-    }
-}
